af6756d953f022fb015ca051ab74d0c8adf419d7
swnordstrom <swnordstrom@email.wm.edu>
2024-06-21 14:30:46 -0700

    add code for first look at growth-survival kernels

--- 03_construct_kernels/growth_survival_kernels.R
+++ 03_construct_kernels/growth_survival_kernels.R
@@ -20,7 +20,8 @@ library(lme4)
 rm(list = ls())
 
 # Load in processed demo data
-demo = read.csv('01_data_cleaning/out/demo_imputed_survival.csv')
+# demo = read.csv('01_data_cleaning/out/demo_imputed_survival.csv')
+demo = read.csv('01_data_cleaning/out/Lomatium_demo_2016-2024.csv')
 
 # Create demo dataset for survival
 # we'll say that survival in year $t$ means surviving from $t$ to $t+1$
@@ -45,9 +46,7 @@ demo.for.surv.sizes = demo.for.surv %>%
   # Remove plants iwth no measurements and plants in 2016
   filter(!is.na(No.leaves.t) & !is.na(Leaf.length.t) & No.leaves.t > 0, Year > 2016) %>%
   # add a size column
-  mutate(size.t = log(No.leaves.t * Leaf.length.t)) %>%
-  # Change year to factor
-  mutate(Year = factor(Year))
+  mutate(size.t = log(No.leaves.t * Leaf.length.t))
 
 # Get a dataset of demo for growth - subsetting the data frame above
 # to get this, we need plants that survived *and* have measurements
@@ -56,39 +55,68 @@ demo.for.growth = demo.for.surv.sizes %>%
   # add a size column
   mutate(size.tp1 = log(No.leaves.tp1 * Leaf.length.tp1))
 
+# Remove 2023 from surv because of NAs (plants seen in 2023 but not 2024 -
+# unsure if alive or just missed)
+demo.for.surv = demo.for.surv %>% 
+  filter(!Year %in% 2023) %>%
+  # Change year to factor
+  mutate(Year = factor(Year))
+
+demo.for.surv.sizes = demo.for.surv.sizes %>% 
+  filter(!Year %in% 2023) %>%
+  # Change year to factor
+  mutate(Year = factor(Year))
+
+# Remove plot 2 in 2024 from growth (because of censoring
+demo.for.growth = demo.for.growth %>% filter(!(Year %in% 2023 & Plot %in% 2)) %>%   # Change year to factor
+  mutate(Year = factor(Year))
+
 # ------------------------------------------------
 # Fit survival models
 
-# First question: do we need size
+# First question: do we need size and/or year
 # (I suspect we will)
 
 s_0 = glmmTMB(
-  formula = surv ~ (1 | Year) + (1 | Plot / plantid),
+  formula = surv ~ (1 | Plot),
+  family = 'binomial',
+  data = demo.for.surv.sizes
+)
+
+s_y = glmmTMB(
+  formula = surv ~ (1 | Year) + (1 | Plot),
   family = 'binomial',
   data = demo.for.surv.sizes
 )
 
 s_s = glmmTMB(
-  formula = surv ~ size.t + (1 | Year) + (1 | Plot / plantid),
+  formula = surv ~ size.t + (1 | Plot),
   family = 'binomial',
   data = demo.for.surv.sizes
 )
 
-AIC(s_0, s_s)
+s_sy = glmmTMB(
+  formula = surv ~ size.t + (1 | Year) + (1 | Plot),
+  family = 'binomial',
+  data = demo.for.surv.sizes
+)
+
+AIC(s_0, s_s, s_y, s_sy)
 # Unsurprisingly, size effect present.
+# BUT, amazingly, the year effect is negligible (!)
 
-# Also check for random effects vs. fixed effects?
-
-s_s_fix = glmmTMB(
-  formula = surv ~ size.t + Year + (1 | Plot / plantid),
-  family = 'binomial',
-  data = demo.for.surv.sizes
-)
-
-AIC(s_s, s_s_fix)
-# Fixed effects model performs slightly better
-# BUT in looking for inter-annual variation it just means too many parameters
-# So continue with fixed effects
+# # Also check for random effects vs. fixed effects?
+# 
+# s_s_fix = glmmTMB(
+#   formula = surv ~ size.t + Year + (1 | Plot),
+#   family = 'binomial',
+#   data = demo.for.surv.sizes
+# )
+# 
+# AIC(s_s, s_s_fix)
+# # Hmm... when removing individual-level random effects in survival model,
+# # random effect-year model out-performs the fixed effect year model
+# # (good!)
 
 # Models to test here:
 # - treatment effect
@@ -97,14 +125,18 @@ AIC(s_s, s_s_fix)
 # - (model will not converge for treatment effect varying by year)
 
 surv.mod.forms = c(
-  'surv ~ size.t + (1 | Year)',
-  'surv ~ size.t + trt + (1 | Year)',
-  'surv ~ size.t * trt + (1 | Year)',
-  'surv ~ (size.t | Year)',
-  'surv ~ trt + (size.t | Year)' ,
-  'surv ~ size.t + trt + (1 | Year) + (1 | Year:trt)'
+  'surv ~ size.t',
+  'surv ~ size.t + trt',
+  'surv ~ size.t * trt'
+  # old code for models with year effects (no longer supported)
+  # 'surv ~ size.t + (1 | Year)',
+  # 'surv ~ size.t + trt + (1 | Year)',
+  # 'surv ~ size.t * trt + (1 | Year)',
+  # 'surv ~ (size.t | Year)',
+  # 'surv ~ trt + (size.t | Year)' ,
+  # 'surv ~ size.t + trt + (1 | Year) + (1 | Year:trt)'
 ) %>%
-  paste('+ (1 | Plot / plantid)')
+  paste('+ (1 | Plot)')
 
 surv.mod.list = vector('list', length = length(surv.mod.forms))
 
@@ -117,48 +149,41 @@ for (j in 1:length(surv.mod.forms)) {
     control = glmerControl(optimizer = 'bobyqa')
   )
 }
-# No convergence issues here
+# Convergence issues... (come on man how does this happen?)
 
 data.frame(
   AIC = sapply(surv.mod.list, AIC) %>% unlist(),
   form = surv.mod.forms
 ) %>%
   mutate(daic = round(AIC - min(AIC), 2))
-# Best model has year-varying treatment effect
-# But it's barely better than the "null" model
+# Best supported model has no treatment effects present
 
-# Okay, but now the fixed effects model is still the best-performing one.
-
-s_s = glmer(
-  formula = surv ~ size.t + (1 | Year) + (1 | Plot / plantid),
-  family = 'binomial',
-  data = demo.for.surv.sizes
-)
 
 # Plot model outputs:
-expand.grid(size.t = (5:60)/10, Year = factor(2017:2022)) %>%
+# expand.grid(size.t = (5:60)/10, Year = factor(2017:2022)) %>%
+expand.grid(size.t = (5:60)/10) %>%
   mutate(
-    pred_y = predict(
-      s_s, newdata = ., type = 'response',
-      re.form = ~ (1 | Year), allow.new.levels = TRUE
-    ),
+    # pred_y = predict(
+    #   s_s, newdata = ., type = 'response',
+    #   re.form = ~ (1 | Year), allow.new.levels = TRUE
+    # ),
     pred = predict(
       s_s, newdata = ., type = 'response',
       re.form = ~ 0, allow.new.levels = TRUE
     )
   ) %>%
-  pivot_longer(c(pred_y, pred), names_to = 'model', values_to = 'pred') %>%
+  # pivot_longer(c(pred_y, pred), names_to = 'model', values_to = 'pred') %>%
   ggplot(aes(x = size.t, y = pred)) +
   geom_point(
     data = demo.for.surv.sizes,
     aes(x = size.t, y = as.numeric(surv)),
     size = 3, alpha = 0.1
   ) +
-  geom_line(aes(linetype = model)) +
+  # geom_line(aes(linetype = model)) +
+  geom_line() +
   labs(x = 'size', y = 'survival') +
   facet_wrap(~ Year)
-# Ah right... a lot of behavior here being driven by plants in the tail.
-# Okay, well, that happens sometimes.
+# Looks good/plausible to me
 
 # ------------------------------------------------
 # Fit growth models
@@ -173,7 +198,8 @@ grow.mod.forms = c(
   'size.tp1 ~ trt + (size.t | Year)',
   'size.tp1 ~ size.t + trt + (1 | Year) + (1 | Year:trt)',
   'size.tp1 ~ trt + (size.t | Year) + (1 | Year:trt)',
-  'size.tp1 ~ trt + (size.t | Year) + (size.t | Year:trt)'
+  'size.tp1 ~ trt + (size.t | Year) + (size.t | Year:trt)',
+  'size.tp1 ~ size.t * trt + (1 | Year) + (1 | Year:trt)'
 ) %>%
   paste('+ (1 | Plot / plantid)')
 
@@ -187,7 +213,7 @@ for (j in 1:length(grow.mod.forms)) {
     control = lmerControl(optimizer = 'bobyqa')
   )
 }
-# Quick!
+# Quick! Thank you Gaussian link!
 
 data.frame(
   AIC = sapply(grow.mod.list, AIC) %>% unlist(),
@@ -200,19 +226,19 @@ data.frame(
 # Only vital rate I have seen like this!
 # Furthermore definitely no support for year-varying size effects
 
-g_s_ty = lmer(
-  size.tp1 ~ size.t + trt + (1 | Year) + (1 | Year:trt) + (1 | Plot / plantid),
+g_st_ty = lmer(
+  size.tp1 ~ size.t * trt + (1 | Year) + (1 | Year:trt) + (1 | Plot / plantid),
   data = demo.for.growth,
   control = lmerControl(optimizer = 'bobyqa')
 )
 
-summary(g_s_ty)
+summary(g_st_ty)
 # Treatment effects are incredibly small, but...
-# there is a slight benefit to both treatments? lmao.
+# there is a slight benefit to both treatments... lol
 
 # Residuals:
 demo.for.growth %>%
-  mutate(resid = residuals(g_s_ty)) %>%
+  mutate(resid = residuals(g_st_ty)) %>%
   ggplot(aes(x = size.t, y = resid)) +
   geom_point() +
   facet_wrap( ~ Year)
@@ -222,7 +248,7 @@ demo.for.growth %>%
 # Not worried about heteroskedasticity here
 
 # The year-treatment random effects
-ranef(g_s_ty)$`Year:trt` %>%
+ranef(g_st_ty)$`Year:trt` %>%
   mutate(year.trt = row.names(.)) %>%
   separate(year.trt, into = c('year', 'trt'), sep = ':') %>%
   rename(intercept = `(Intercept)`) %>%
@@ -232,24 +258,24 @@ ranef(g_s_ty)$`Year:trt` %>%
   scale_colour_manual(values = c('black', 'red', 'blue'))
 # Not seeing clear patterns here
 # Maybe some negative temporal autocorrelation?
-# Controls are pretty noisy though...
+# Controls are pretty noisy though
 
 # Model predictions, year-varying first
 expand.grid(
   size.t = (9:60)/10, 
-  Year = factor(2017:2022), 
+  Year = factor(2017:2023), 
   trt = c('control', 'drought', 'irrigated')
 ) %>%
   mutate(
     pred_y = predict(
-      g_s_ty,
+      g_st_ty,
       newdata = .,
       re.form = ~ (1 | Year),
       type = 'response',
       allow.new.levels = TRUE
     ),
     pred_mean = predict(
-      g_s_ty,
+      g_st_ty,
       newdata = .,
       re.form = ~ 0,
       type = 'response',
@@ -270,6 +296,55 @@ expand.grid(
   facet_wrap(~ Year)
 # That 2020 fit looks bad... maybe it's a plot-variance effect?
 # Yeesh.
+
+# Ignoring year effects
+expand.grid(
+  size.t = (9:60)/10, 
+  Year = factor(2017:2023), 
+  trt = c('control', 'drought', 'irrigated')
+) %>%
+  mutate(
+    pred = predict(
+      g_st_ty,
+      newdata = .,
+      re.form = ~ 0,
+      type = 'response',
+      allow.new.levels = TRUE
+    )
+  ) %>%
+  # pivot_longer(c(pred_mean, pred_y), names_to = 'model', values_to = 'pred') %>%
+  ggplot(aes(x = size.t, y = pred)) +
+  geom_point(
+    data = demo.for.growth,
+    aes(x = size.t, y = size.tp1, colour = trt),
+    inherit.aes = FALSE,
+    size = 3, alpha = 0.1
+  ) +
+  geom_segment(aes(x = .9, xend = 6, y = .9, yend = 6), linetype = 2, colour = 'gray77') +
+  geom_line(aes(colour = trt)) +
+  scale_colour_manual(values = c('black', 'red', 'blue'))
+# Still looks from this like the overall effect of drought is... positive
+# Larger plants will shrink less with abundant water...
+
+expand.grid(
+  size.t = (9:60)/10, 
+  Year = factor(2017:2023), 
+  trt = c('control', 'drought', 'irrigated')
+) %>%
+  mutate(
+    pred_y = predict(
+      g_st_ty,
+      newdata = .,
+      re.form = ~ (1 | Year),
+      type = 'response',
+      allow.new.levels = TRUE
+    )
+  ) %>%
+  ggplot(aes(x = size.t, y = pred_y)) +
+  geom_segment(aes(x = .9, xend = 6, y = .9, yend = 6), linetype = 2, colour = 'gray77') +
+  geom_line(aes(colour = trt, group = interaction(Year, trt))) +
+  scale_colour_manual(values = c('black', 'red', 'blue'))
+# Hmm... oay not a super cool plot...
 
 # ------------------------------------------------
 # Construct kernels
