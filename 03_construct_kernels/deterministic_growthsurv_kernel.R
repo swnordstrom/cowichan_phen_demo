456a6a5eb76119a7652c4be8eae79e5d84256911
swnordstrom <swnordstrom@email.wm.edu>
2024-07-17 16:14:09 -0700

    re-export G/S kernel with better model and comment out model selection

--- 03_construct_kernels/deterministic_growthsurv_kernel.R
+++ 03_construct_kernels/deterministic_growthsurv_kernel.R
@@ -79,197 +79,280 @@ demo.grow = demo.surv.sizes %>%
 # Finally: subset surv dataset to not include 2023-2024 surv
 demo.surv.sizes = demo.surv.sizes %>% filter(surv.year < 2024)
 
-###
+### ----- MODEL SELECTION -----
+
+# # ------------------------------------------------
+# # Fit survival models
+# 
+# # First question: do we need size and/or year
+# # (I suspect we will)
+# 
+# s_0 = glmmTMB(
+#   formula = surv ~ (1 | Plot),
+#   family = 'binomial',
+#   data = demo.surv.sizes
+# )
+# 
+# s_y = glmmTMB(
+#   formula = surv ~ (1 | surv.year) + (1 | Plot),
+#   family = 'binomial',
+#   data = demo.surv.sizes
+# )
+# 
+# s_s = glmmTMB(
+#   formula = surv ~ size.prev + (1 | Plot),
+#   family = 'binomial',
+#   data = demo.surv.sizes
+# )
+# 
+# s_s_y = glmmTMB(
+#   formula = surv ~ size.prev + (1 | surv.year) + (1 | Plot),
+#   family = 'binomial',
+#   data = demo.surv.sizes
+# )
+# 
+# AIC(s_0, s_y, s_s, s_s_y) %>% mutate(daic = round(AIC - min(AIC), 2))
+# 
+# # Interesting! No support for year effects.
+# 
+# summary(s_s)
+# # Increasing size increases odds of survival, unsurprisingly!
+# 
+# # Models to test:
+# # - Treatment effect
+# # - Size-treatment interaciton
+# 
+# s_t = glmmTMB(
+#   formula = surv ~ size.prev + trt + (1 | Plot),
+#   family = 'binomial',
+#   data = demo.surv.sizes
+# )
+# 
+# s_ts = glmmTMB(
+#   formula = surv ~ size.prev * trt + (1 | Plot),
+#   family = 'binomial',
+#   data = demo.surv.sizes
+# )
+# 
+# AIC(s_s, s_t, s_ts) %>% mutate(daic = round(AIC - min(AIC), 2))
+# # No treatment effects.
+# 
+# # Plot of this model:
+# surv.preds = data.frame(size.prev = (5:60) / 10) %>%
+#   mutate(pred = predict(s_s, re.form = ~ 0, newdata = ., allow.new.levels = TRUE, type = 'response'))
+# 
+# head(surv.preds)
+# 
+# demo.surv.sizes %>%
+#   mutate(surv = as.numeric(surv)) %>%
+#   ggplot(aes(x = size.prev, y = surv)) +
+#   geom_point(position = position_jitter(height = 0.2), alpha = 0.1, size = 3) +
+#   geom_line(data = surv.preds, aes(y = pred))
+# 
+# # # Random effects:
+# 
+# # Plot level effects:
+# data.frame(ranef(s_s_y)$cond$Plot) %>%
+#   mutate(Plot = row.names(.)) %>%
+#   rename(incp = `X.Intercept.`) %>%
+#   merge(distinct(demo.surv.sizes, Plot, trt)) %>%
+#   mutate(Plot = factor(Plot)) %>%
+#   ggplot(aes(x = Plot, y = incp, colour = trt)) +
+#   geom_point(size = 4) +
+#   scale_colour_manual(values = c('black', 'red', 'blue'))
+# # Definitely not seeing a clear pattern here
+# 
+# # Maybe slightly higher intercepts for irrigated but I trust the models
+# # suggesting no treatment effect
+# 
+# 
+# # ------------------------------------------------
+# # Fit growth models
+# 
+# # Test for year effects
+# 
+# g_0 = glmmTMB(
+#   size.cur ~ size.prev + (1 | Plot / plantid),
+#   data = demo.grow
+# )
+# 
+# g_y = glmmTMB(
+#   size.cur ~ size.prev + (1 | prev.year) + (1 | Plot / plantid),
+#   data = demo.grow
+# )
+# 
+# AIC(g_y, g_0)
+# # Yep. Support for a year effect.
+# 
+# # Treatment effect
+# 
+# g_t = glmmTMB(
+#   size.cur ~ size.prev + trt + (1 | prev.year) + (1 | Plot / plantid),
+#   data = demo.grow
+# )
+# 
+# AIC(g_t, g_y)
+# # Wow... no treatment effect.
+# 
+# # There are a couple of other things to try:
+# # - treatment-year
+# # - treatment-size
+# # - size-year
+# 
+# g_ty = glmmTMB(
+#   size.cur ~ size.prev + trt + (1 | prev.year) + (1 | prev.year:trt) + (1 | Plot / plantid),
+#   data = demo.grow
+# )
+# 
+# g_st.ty = glmmTMB(
+#   size.cur ~ size.prev + size.prev * trt + (1 | prev.year) + (1 | prev.year:trt) + (1 | Plot / plantid),
+#   data = demo.grow
+# )
+# 
+# g_st = glmmTMB(
+#   size.cur ~ size.prev * trt + (1 | prev.year) + (1 | Plot / plantid),
+#   data = demo.grow
+# )
+# 
+# g_sy = glmmTMB(
+#   size.cur ~ size.prev + (size.prev | prev.year) + (1 | Plot / plantid),
+#   data = demo.grow
+# )
+# 
+# AIC(g_y, g_t, g_ty, g_st.ty, g_st, g_sy) %>% mutate(daic = round(AIC - min(AIC), 2))
+# # Yep - as before, overwhelming support for the year-treatment interaction
+# 
+# summary(g_st.ty)
+# summary(g_ty)
+# # These feel like *very* weak effects on average...
+# # (We also have slight advantages to both drought and irrigation...)
+# 
+# # glmmTMB won't let me generate predictions with half-specified random effects
+# data.frame(ranef(g_st.ty)$cond$`prev.year:trt`) %>%
+#   rename(intcp = `X.Intercept.`) %>%
+#   mutate(year.trt = row.names(.)) %>%
+#   separate(year.trt, into = c('year', 'trt'), sep = ':') %>%
+#   ggplot(aes(x = year, y = intcp, colour = trt, group = trt)) +
+#   geom_line() +
+#   scale_colour_manual(values = c('black', 'red', 'blue'))
+# # Hmm... yep pretty noisy  
+# # Although if the mean effect of drought is +.12... that's actually kinda big
+# # compared to the magnitude of fluctuations?
+# 
+# # What would results look like if I made predictions for the median plant+plot?
+# abs(ranef(g_st.ty)$cond$Plot) %>% arrange(`(Intercept)`) # median plot is 13, eff is +.0222
+# ranef(g_st.ty)$cond$`plantid:Plot` %>% 
+#   mutate(pla = row.names(.)) %>% 
+#   filter(grepl('\\_13\\:', pla)) %>% 
+#   mutate(eff = `(Intercept)` + .0222) %>% 
+#   arrange(abs(eff))
+# # seems like median plant is our old friend 3989_13:13
+# 
+# expand.grid(
+#   size.prev = (5:60)/10,
+#   prev.year = factor(2017:2023),
+#   trt = c('control', 'drought', 'irrigated')
+# ) %>%
+#   mutate(Plot = 13, plantid = '3989_13') %>%
+#   mutate(pp = predict(g_st.ty, newdata = ., re.form = NULL)) %>%
+#   ggplot(aes(x = size.prev, y = pp, colour = trt, group = trt)) +
+#   geom_segment(aes(x = 0.5, y = 0.5, xend = 6, yend = 6), linetype = 2, colour = 'gray77') +
+#   geom_line() +
+#   scale_colour_manual(values = c('black', 'red', 'blue')) +
+#   facet_wrap(~ prev.year)
+# # Usually, but not always, control performing below drought...
+# # (It would be cool to iterate individuals through the years...)
+# 
+# # Compare predictions from our two different models
+# expand.grid(
+#   size.prev = (5:60)/10,
+#   trt = c('control', 'drought', 'irrigated')
+# ) %>%
+#   mutate(
+#     pred.yrf = predict(g_st.ty, newdata = ., re.form = ~ 0, allow.new.levels = TRUE),
+#     pred.nrf = predict(g_st, newdata = ., re.form = ~ 0, allow.new.levels = TRUE)
+#   ) %>%
+#   ggplot(aes(x = pred.nrf, y = pred.yrf, colour = trt)) +
+#   geom_segment(aes(x = 2, y = 2, xend = 4.5, yend = 4.5), linetype = 2, colour = 'gray77') +
+#   geom_point(size = 3, alpha = 0.25) +
+#   labs(x = 'non-year varying', y = 'year-varying') +
+#   scale_colour_manual(values = c('black', 'red', 'blue'))
+# # above diagonal: greater predicted growth in year-varying model
+# # below diagonal: greater predicted growth in non-year varying model
+# # there's the *least* change in the control treatment...
+# 
+# expand.grid(
+#   size.prev = (5:60)/10,
+#   trt = c('control', 'drought', 'irrigated')
+# ) %>%
+#   mutate(
+#     pred.yrf = predict(g_st.ty, newdata = ., re.form = ~ 0, allow.new.levels = TRUE),
+#     pred.nrf = predict(g_st, newdata = ., re.form = ~ 0, allow.new.levels = TRUE)
+#   ) %>%
+#   pivot_longer(cols = c(pred.yrf, pred.nrf), names_to = 'model', values_to = 'pred') %>%
+#   ggplot(aes(x = size.prev, y = pred, group = interaction(trt, model), colour = trt)) +
+#   geom_segment(aes(x = .5, y = .5, xend = 6, yend = 6), linetype = 2, colour = 'gray77') +
+#   geom_line(aes(linetype = model)) +
+#   labs(x = 'non-year varying', y = 'year-varying') +
+#   scale_colour_manual(values = c('black', 'red', 'blue'))
+# # The model with the year-varying effects has less shrinkage for large plants, less growth for small plants
+# 
+# # I just don't trust these random effects
+# AIC(g_y, g_st, g_sy) %>% mutate(daic = round(AIC - min(AIC), 2))
+# 
+# # Out of these, g_st appears to be best supported
+# summary(g_st.ty)
+# 
+# expand.grid(trt = c('control', 'drought', 'irrigated'), size.prev = (5:60)/10) %>%
+#   mutate(size.cur = predict(g_st.ty, re.form = ~ 0, newdata = ., allow.new.levels = TRUE)) %>%
+#   ggplot(aes(x = size.prev, y = size.cur, colour = trt, group = trt)) +
+#   geom_segment(aes(x = .5, y = .5, xend = 6, yend = 6), linetype = 2, linewidth = 0.25) +
+#   geom_point(data = demo.grow, size = 3, alpha = 0.1) +
+#   geom_line() +
+#   scale_colour_manual(values = c('black', 'red', 'blue')) +
+#   facet_wrap(~ trt)
+# # Hmm...
+# 
+# # Look at residuals
+# demo.grow %>%
+#   mutate(resids = residuals(g_st.ty)) %>%
+#   ggplot(aes(x = size.prev, y = resids, colour = trt)) +
+#   geom_segment(aes(x = .5, xend = 6, y = 0, yend = 0), colour = 'gray', linetype = 2, linewidth = 0.5) +
+#   scale_colour_manual(values = c('black' ,'red', 'blue')) +
+#   geom_point(alpha = 0.1, size = 3)
+# # Eh... a few outliers for medium-large plants...
+# 
+# demo.grow %>%
+#   mutate(resids = residuals(g_st)) %>%
+#   ggplot(aes(x = prev.year, y = resids)) +
+#   geom_segment(aes(x = 2017, xend = 2023, y = 0, yend = 0), linetype = 2, linewidth = 0.5) +
+#   geom_point(position = position_jitter(width = 0.4), alpha = 0.05, size = 3)
+# # A few negative outliers in 2022, 2023 but nothing awful.
 
 # ------------------------------------------------
-# Fit survival models
+# Construct kernels
 
-# First question: do we need size and/or year
-# (I suspect we will)
+# --- Refit final models
 
-s_0 = glmmTMB(
-  formula = surv ~ (1 | Plot),
-  family = 'binomial',
-  data = demo.surv.sizes
-)
-
-s_y = glmmTMB(
-  formula = surv ~ (1 | surv.year) + (1 | Plot),
-  family = 'binomial',
-  data = demo.surv.sizes
-)
-
+# Survival model
 s_s = glmmTMB(
   formula = surv ~ size.prev + (1 | Plot),
   family = 'binomial',
   data = demo.surv.sizes
 )
 
-s_s_y = glmmTMB(
-  formula = surv ~ size.prev + (1 | surv.year) + (1 | Plot),
-  family = 'binomial',
-  data = demo.surv.sizes
-)
-
-AIC(s_0, s_y, s_s, s_s_y) %>% mutate(daic = round(AIC - min(AIC), 2))
-
-# Interesting! No support for year effects.
-
-summary(s_s)
-# Increasing size increases odds of survival, unsurprisingly!
-
-# Models to test:
-# - Treatment effect
-# - Size-treatment interaciton
-
-s_t = glmmTMB(
-  formula = surv ~ size.prev + trt + (1 | Plot),
-  family = 'binomial',
-  data = demo.surv.sizes
-)
-
-s_ts = glmmTMB(
-  formula = surv ~ size.prev * trt + (1 | Plot),
-  family = 'binomial',
-  data = demo.surv.sizes
-)
-
-AIC(s_s, s_t, s_ts) %>% mutate(daic = round(AIC - min(AIC), 2))
-# No treatment effects.
-
-# Plot of this model:
-surv.preds = data.frame(size.prev = (5:60) / 10) %>%
-  mutate(pred = predict(s_s, re.form = ~ 0, newdata = ., allow.new.levels = TRUE, type = 'response'))
-
-head(surv.preds)
-
-demo.surv.sizes %>%
-  mutate(surv = as.numeric(surv)) %>%
-  ggplot(aes(x = size.prev, y = surv)) +
-  geom_point(position = position_jitter(height = 0.2), alpha = 0.1, size = 3) +
-  geom_line(data = surv.preds, aes(y = pred))
-
-# # Random effects:
-
-# Plot level effects:
-data.frame(ranef(s_s_y)$cond$Plot) %>%
-  mutate(Plot = row.names(.)) %>%
-  rename(incp = `X.Intercept.`) %>%
-  merge(distinct(demo.surv.sizes, Plot, trt)) %>%
-  mutate(Plot = factor(Plot)) %>%
-  ggplot(aes(x = Plot, y = incp, colour = trt)) +
-  geom_point(size = 4) +
-  scale_colour_manual(values = c('black', 'red', 'blue'))
-# Definitely not seeing a clear pattern here
-
-# Maybe slightly higher intercepts for irrigated but I trust the models
-# suggesting no treatment effect
-
-
-# ------------------------------------------------
-# Fit growth models
-
-# Test for year effects
-
-g_0 = glmmTMB(
-  size.cur ~ size.prev + (1 | Plot / plantid),
+# Growth model
+g_st.ty = glmmTMB(
+  size.cur ~ size.prev + size.prev * trt + (1 | prev.year) + (1 | prev.year:trt) + (1 | Plot / plantid),
   data = demo.grow
 )
 
-g_y = glmmTMB(
-  size.cur ~ size.prev + (1 | prev.year) + (1 | Plot / plantid),
-  data = demo.grow
-)
-
-AIC(g_y, g_0)
-# Yep. Support for a year effect.
-
-# Treatment effect
-
-g_t = glmmTMB(
-  size.cur ~ size.prev + trt + (1 | prev.year) + (1 | Plot / plantid),
-  data = demo.grow
-)
-
-AIC(g_t, g_y)
-# Wow... no treatment effect.
-
-# There are a couple of other things to try:
-# - treatment-year
-# - treatment-size
-# - size-year
-
-g_ty = glmmTMB(
-  size.cur ~ size.prev + trt + (1 | prev.year) + (1 | prev.year:trt) + (1 | Plot / plantid),
-  data = demo.grow
-)
-
-g_st = glmmTMB(
-  size.cur ~ size.prev * trt + (1 | prev.year) + (1 | Plot / plantid),
-  data = demo.grow
-)
-
-g_sy = glmmTMB(
-  size.cur ~ size.prev + (size.prev | prev.year) + (1 | Plot / plantid),
-  data = demo.grow
-)
-
-AIC(g_y, g_t, g_ty, g_st, g_sy) %>% mutate(daic = round(AIC - min(AIC), 2))
-# Yep - as before, overwhelming support for the year-treatment interaction
-
-summary(g_ty)
-# These feel like *very* weak effects on average...
-# (We also have slight advantages to both drought and irrigation...)
-
-# glmmTMB won't let me generate predictions with half-specified random effects
-data.frame(ranef(g_ty)$cond$`prev.year:trt`) %>%
-  rename(intcp = `X.Intercept.`) %>%
-  mutate(year.trt = row.names(.)) %>%
-  separate(year.trt, into = c('year', 'trt'), sep = ':') %>%
-  ggplot(aes(x = year, y = intcp, colour = trt, group = trt)) +
-  geom_line() +
-  scale_colour_manual(values = c('black', 'red', 'blue'))
-# Hmm... yep pretty noisy  
-# Although if the mean effect of drought is +.12... that's actually kinda big
-# compared to the magnitude of fluctuations?
-  
-# I just don't trust these random effects
-AIC(g_y, g_st, g_sy) %>% mutate(daic = round(AIC - min(AIC), 2))
-
-# Out of these, g_st appears to be best supported
-summary(g_st)
-
-expand.grid(trt = c('control', 'drought', 'irrigated'), size.prev = (5:60)/10) %>%
-  mutate(size.cur = predict(g_st, re.form = ~ 0, newdata = ., allow.new.levels = TRUE)) %>%
-  ggplot(aes(x = size.prev, y = size.cur, colour = trt, group = trt)) +
-  geom_segment(aes(x = .5, y = .5, xend = 6, yend = 6), linetype = 2, linewidth = 0.25) +
-  geom_point(data = demo.grow, size = 3, alpha = 0.1) +
-  geom_line() +
-  scale_colour_manual(values = c('black', 'red', 'blue'))
-# Hmm...
-
-# Look at residuals
-demo.grow %>%
-  mutate(resids = residuals(g_st)) %>%
-  ggplot(aes(x = size.prev, y = resids)) +
-  geom_segment(aes(x = .5, xend = 6, y = 0, yend = 0), linetype = 2, linewidth = 0.5) +
-  geom_point(alpha = 0.1, size = 3)
-# Looks fine - maybe something off with one of those tails
-
-demo.grow %>%
-  mutate(resids = residuals(g_st)) %>%
-  ggplot(aes(x = prev.year, y = resids)) +
-  geom_segment(aes(x = 2017, xend = 2023, y = 0, yend = 0), linetype = 2, linewidth = 0.5) +
-  geom_point(position = position_jitter(width = 0.4), alpha = 0.05, size = 3)
-# A few negative outliers in 2022, 2023 but nothing awful.
-
-# --- 
-
-# ------------------------------------------------
-# Construct kernels
+# --- Extract parameters needed
 
 # Residual variance in growth models
-grow.sd = summary(g_st)$sigma
+grow.sd = summary(g_st.ty)$sigma
+
+# --- Construct and work with data frame
 
 # Get a scaffold
 grow.surv.kernel = expand.grid(
@@ -291,7 +374,7 @@ grow.surv.kernel = grow.surv.kernel %>%
   mutate(
     pred.grow.mean = predict(
       newdata = .,
-      object = g_st, type = 'response',
+      object = g_st.ty, type = 'response',
       re.form = ~ 0, allow.new.levels = TRUE
     )
   ) %>%
