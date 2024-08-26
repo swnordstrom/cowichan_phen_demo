5a55cfc9b0a051ae18f1b16b8d56517ab100c662
swnordstrom <swnordstrom@email.wm.edu>
2024-08-26 12:17:19 -0700

    update demo-reproduction setup script to include seed and phen datasets

--- 03_construct_kernels/prepare_demo_data_repr.R
+++ 03_construct_kernels/prepare_demo_data_repr.R
@@ -34,19 +34,19 @@ demo.seed = demo.flow %>%
   filter(Year > 2020, phen.umbels > 0) %>%
   filter(in.seed)
 
-# Need to add in rows for umbels that died before the seed counting
-# This is necessary for estimating the probability of an umbel producing zero seeds
-demo.seed = rbind(
-  demo.seed,
-  demo.seed %>%
-    group_by(Year, plantid) %>%
-    mutate(miss.umbel = ifelse(phen.umbels < n(), 0, phen.umbels - n())) %>%
-    ungroup() %>%
-    distinct(Year, plantid, .keep_all = TRUE) %>%
-    uncount(miss.umbel) %>%
-    mutate(no.seeds = 0)
-) %>%
-  mutate(Year = factor(Year))
+# # Need to add in rows for umbels that died before the seed counting
+# # This is necessary for estimating the probability of an umbel producing zero seeds
+# demo.seed = rbind(
+#   demo.seed,
+#   demo.seed %>%
+#     group_by(Year, plantid) %>%
+#     mutate(miss.umbel = ifelse(phen.umbels < n(), 0, phen.umbels - n())) %>%
+#     ungroup() %>%
+#     distinct(Year, plantid, .keep_all = TRUE) %>%
+#     uncount(miss.umbel) %>%
+#     mutate(no.seeds = 0)
+# ) %>%
+#   mutate(Year = factor(Year))
 
 # Dataset for estimating recruit size distribution
 demo.recr = all.data %>%
@@ -73,3 +73,48 @@ demo.flow = demo.flow %>%
   distinct(Year, plantid, .keep_all = TRUE) %>%
   # Remove plot 2 in 2024 (missing a lot of vegetative plant sizes)
   filter(!(Plot %in% 2 & Year %in% 2024))
+
+# Umbel-level budding phenology data
+phen = all.data %>% 
+  filter(in.phen) %>%
+  # Split out the bud dates for bud date models; the most umbels seen in a
+  # plant is 12, so use separate() to kick these out and then pivot_long to get
+  # one row per umbel
+  # (first - need to get one row per plant - do a distinct())
+  distinct(Year, plantid, .keep_all = TRUE) %>%
+  separate_wider_delim(phen.julis, names = paste0('uu', 1:12), delim = ';', too_few = 'align_start') %>%
+  pivot_longer(starts_with('uu'), names_to = 'umbel.number', values_to = 'phen.julian') %>%
+  filter(!is.na(phen.julian)) %>%
+  mutate(phen.julian = as.numeric(gsub('\\s', '', phen.julian))) %>%
+  mutate(Year = factor(Year))
+
+# All plants in phen, seed, and demo
+seed = all.data %>% 
+  filter(in.phen, in.seed, in.demo) %>%
+  # Need size data
+  filter(!is.na(No.leaves), !is.na(Leaf.length), No.leaves > 0, Leaf.length > 0) %>%
+  # Hmm... okay, separae() and across() doesn't work, so I guess I can merge
+  # this in with the data frame above, summarised by mean phen date per plant
+  merge(
+    y = phen %>% 
+      group_by(plantid, Year) %>% 
+      summarise(
+        mean.phen = mean(phen.julian, na.rm = TRUE), 
+        sept.phen = length(unique(phen.julian))
+      ) %>%
+      ungroup(),
+    all.x = TRUE, all.y = FALSE
+  )
+
+seed = rbind(
+  seed,
+  seed %>%
+    group_by(Year = factor(Year), plantid) %>%
+    mutate(miss.umbel = ifelse(phen.umbels < n(), 0, phen.umbels - n())) %>%
+    ungroup() %>%
+    distinct(Year, plantid, .keep_all = TRUE) %>%
+    uncount(miss.umbel) %>%
+    mutate(no.seeds = 0)
+) %>%
+  mutate(phen.c = mean.phen - round(mean(mean.phen))) %>%
+  mutate(size = log(No.leaves * Leaf.length))
