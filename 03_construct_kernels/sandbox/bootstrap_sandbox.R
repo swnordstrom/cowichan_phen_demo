0b698001ce0a053749f0fd689699d16d069c40f7
swnordstrom <swnordstrom@email.wm.edu>
2024-08-26 12:14:34 -0700

    add some code and co mments to bootstrap sandbox script

--- 03_construct_kernels/sandbox/bootstrap_sandbox.R
+++ 03_construct_kernels/sandbox/bootstrap_sandbox.R
@@ -75,53 +75,53 @@ demo.grow = demo.surv.sizes %>%
 # Finally: subset surv dataset to not include 2023-2024 surv
 demo.surv.sizes = demo.surv.sizes %>% filter(surv.year < 2024)
 
-# --- Boots ---------------------------------------------------------
-
-set.seed(11225)
-
-# Number of bootstraps
-n.straps = 500
-
-# Set up the bootstraps
-
-surv.boots = demo.surv.sizes %>%
-  sample_n(size = n.straps * nrow(.), replace = TRUE) %>%
-  mutate(samp = rep_len(1:n.straps, length.out = nrow(.))) %>%
-  split(.$samp) %>%
-  mclapply(function(df) {
-    glmmTMB(
-      formula = surv ~ size.prev + (1 | Plot),
-      family = 'binomial',
-      data = df
-    ) %>%
-      (function(mod) mod$fit$par)
-    },
-    mc.cores = 6
-  ) %>%
-  do.call(rbind, .) %>%
-  data.frame() %>%
-  mutate(mod = 'surv', i = 1:n.straps) %>%
-  select(mod, i, everything())
-
-grow.boots = demo.grow %>%
-  sample_n(size = n.straps * nrow(.), replace = TRUE) %>%
-  mutate(samp = rep_len(1:n.straps, length.out = nrow(.))) %>%
-  split(.$samp) %>%
-  mclapply(
-    function(df) {
-      glmmTMB(
-        size.cur ~ size.prev + size.prev * trt + (1 | prev.year) + (1 | prev.year:trt) + (1 | Plot / plantid),
-        data = df
-      ) %>%
-        (function(mod) c(mod$fit$par, sigma = summary(mod)$sigma))
-      },
-    mc.cores = 6
-  ) %>%
-  # lapply(function(mod) c(mod$fit$par, sigma = summary(mod)$sigma)) %>%
-  do.call(rbind, .) %>%
-  data.frame() %>%
-  mutate(mod = 'grow', i = 1:n.straps) %>%
-  select(mod, i, sigma, everything())
+# # --- Boots ---------------------------------------------------------
+# # OLD boots without preserved sampling
+# set.seed(11225)
+# 
+# # Number of bootstraps
+# n.straps = 100
+# 
+# # Set up the bootstraps
+# 
+# surv.boots = demo.surv.sizes %>%
+#   sample_n(size = n.straps * nrow(.), replace = TRUE) %>%
+#   mutate(samp = rep_len(1:n.straps, length.out = nrow(.))) %>%
+#   split(.$samp) %>%
+#   mclapply(function(df) {
+#     glmmTMB(
+#       formula = surv ~ size.prev + (1 | Plot),
+#       family = 'binomial',
+#       data = df
+#     ) %>%
+#       (function(mod) mod$fit$par)
+#     },
+#     mc.cores = 6
+#   ) %>%
+#   do.call(rbind, .) %>%
+#   data.frame() %>%
+#   mutate(mod = 'surv', i = 1:n.straps) %>%
+#   select(mod, i, everything())
+# 
+# grow.boots = demo.grow %>%
+#   sample_n(size = n.straps * nrow(.), replace = TRUE) %>%
+#   mutate(samp = rep_len(1:n.straps, length.out = nrow(.))) %>%
+#   split(.$samp) %>%
+#   mclapply(
+#     function(df) {
+#       glmmTMB(
+#         size.cur ~ size.prev + size.prev * trt + (1 | prev.year) + (1 | prev.year:trt) + (1 | Plot / plantid),
+#         data = df
+#       ) %>%
+#         (function(mod) c(mod$fit$par, sigma = summary(mod)$sigma))
+#       },
+#     mc.cores = 6
+#   ) %>%
+#   # lapply(function(mod) c(mod$fit$par, sigma = summary(mod)$sigma)) %>%
+#   do.call(rbind, .) %>%
+#   data.frame() %>%
+#   mutate(mod = 'grow', i = 1:n.straps) %>%
+#   select(mod, i, sigma, everything())
 
 # --- Original models -------------------------------------------------------
 
@@ -138,7 +138,82 @@ g_st.ty = glmmTMB(
   data = demo.grow
 )
 
-# Get bootstrapped kernels
+# --- Boots ---------------------------------------------------------
+
+set.seed(11225)
+
+# Number of bootstraps
+n.straps = 100
+
+# Set up the bootstraps
+
+surv.boots = demo.surv.sizes %>%
+  uncount(weights = n.straps) %>%
+  group_by(plantid, surv.year) %>%
+  mutate(samp = 1:n.straps) %>%
+  group_by(Plot, surv.year, samp) %>%
+  sample_n(size = n(), replace = TRUE) %>%
+  ungroup() %>%
+  split(.$samp) %>%
+  mclapply(function(df) {
+    glmmTMB(
+      formula = surv ~ size.prev + (1 | Plot),
+      family = 'binomial',
+      data = df
+    ) %>%
+      (function(mod) mod$fit$par)
+  },
+  mc.cores = 6
+  ) %>%
+  do.call(rbind, .) %>%
+  data.frame() %>%
+  mutate(mod = 'surv', i = 1:n.straps) %>%
+  select(mod, i, everything())
+
+grow.boots = demo.grow %>%
+  uncount(weights = n.straps) %>%
+  group_by(plantid, surv.year) %>%
+  mutate(samp = 1:n.straps) %>%
+  group_by(Plot, surv.year, samp) %>%
+  sample_n(size = n(), replace = TRUE) %>%
+  ungroup() %>%
+  split(.$samp) %>%
+  mclapply(
+    function(df) {
+      glmmTMB(
+        size.cur ~ size.prev + size.prev * trt + (1 | prev.year) + (1 | prev.year:trt) + (1 | Plot / plantid),
+        data = df
+      ) %>%
+        (function(mod) mod$fit$par)
+    },
+    mc.cores = 6
+  ) %>%
+  # lapply(function(mod) c(mod$fit$par, sigma = summary(mod)$sigma)) %>%
+  do.call(rbind, .) %>%
+  data.frame() %>%
+  mutate(mod = 'grow', i = 1:n.straps) %>%
+  select(mod, i, everything())
+
+# # Un-biasing the bootstrapped estimates
+# Manually shifting individual columns of the bootstrap to have the mean of
+# boostrapped samples be identical to the parameter values of the true model
+
+# Survival bootstraps
+surv.boots[,-(1:2)] = surv.boots[,-(1:2)] + matrix(
+  (s_s$fit$par - colMeans(surv.boots[,-(1:2)])), 
+  nrow = n.straps, ncol = length(s_s$fit$par), byrow = TRUE,
+)
+
+grow.boots[,-(1:2)] = grow.boots[,-(1:2)] + matrix(
+  (g_st.ty$fit$par - colMeans(grow.boots[,-(1:2)])), 
+  nrow = n.straps, ncol = length(g_st.ty$fit$par), byrow = TRUE,
+)
+
+# very small differences, all numerical rounding
+mean((colMeans(surv.boots[,-(1:2)]) - s_s$fit$par)^2)
+mean((colMeans(grow.boots[,-(1:2)]) - g_st.ty$fit$par)^2)
+
+# --- Get bootstrapped kernels ------------------------------------
 
 bootstrap.backbone = expand.grid(
   size.prev = (5:60)/10,
@@ -165,12 +240,12 @@ for (i in 1:n.straps) {
       pred.grow.mean = predict(
         newdata = .,
         object = g_st.ty, type = 'response',
-        newparams = grow.boots[i, -(1:3)],
+        newparams = grow.boots[i, -(1:2)],
         re.form = ~ 0, allow.new.levels = TRUE
       )
     ) %>%
     # Predicted distribution of sizes in next time step
-    mutate(p.grow.size = 0.1 * dnorm(size.cur, mean = pred.grow.mean, sd = grow.boots$sigma[i])) %>%
+    mutate(p.grow.size = 0.1 * dnorm(size.cur, mean = pred.grow.mean, sd = sqrt(exp(grow.boots$betad[i])))) %>%
     # Combine all together to get overall size distribution in next time step
     mutate(p.size.cur = pred.surv * p.grow.size) %>%
     select(-c(pred.surv, pred.grow.mean, p.grow.size)) %>%
@@ -184,5 +259,138 @@ do.call(rbind, kernel.list) %>%
   pivot_wider(names_from = boot, values_from = p.size.cur) %>%
   write.csv(
     '03_construct_kernels/out/deterministic_growsurv_bootstrap.csv',
+    row.names = FALSE, na = ''
+  )
+
+# ------
+# ------ Perturbation bootstrapping (repeat above procedure for each boot sample...)
+# ------
+
+# Parameters to perturb:
+# - Survival: nothing (no treatment effects)
+# - Growth:
+#   - Intercept
+#   - Slope
+
+bootstrap.backbone = expand.grid(
+  size.prev = (5:60)/10,
+  size.cur = (5:60)/10,
+  trt = c('control', 'drought', 'irrigated')
+)
+
+# Perturbation amount
+delta = 0.0001
+
+# Get a list for outputs
+gs.pert.boot = vector('list', n.straps)
+
+for (i in 1:n.straps) {
+  
+  # Set up list for outputs
+  this.boot = vector('list', 2)
+  
+  # 1: Growth model intercept
+  
+  this.boot[[1]] = bootstrap.backbone %>%
+    # Predicted survival
+    mutate(
+      pred.surv = predict(
+        newdata = .,
+        object = s_s, type = 'response',
+        newparams = surv.boots[i,-(1:2)],
+        re.form = ~ 0, allow.new.levels = TRUE
+      )
+    ) %>%
+    # Predicted growth
+    mutate(
+      pred.grow.mean = predict(
+        newdata = .,
+        object = g_st.ty, type = 'response',
+        newparams = grow.boots[i, -(1:2)] %>%
+          (function(x) {
+            x[1] <- x[1] + delta
+            return(x)
+          }),
+        re.form = ~ 0, allow.new.levels = TRUE
+      )
+    ) %>%
+    mutate(p.grow.size = 0.1 * dnorm(size.cur, pred.grow.mean, sd = sqrt(exp(grow.boots$betad[i])))) %>%
+    # Combine all together to get overall size distribution in next time step
+    mutate(p.size.cur = pred.surv * p.grow.size) %>%
+    select(-c(pred.surv, pred.grow.mean, p.grow.size)) %>%
+    mutate(
+      perturb.param = 'grow.int'
+      # orig.par.val = case_when(
+      #   trt %in% 'control' ~ grow.boots[i,-(1:3)][1],
+      #   trt %in% 'drought' ~ grow.boots[i,-(1:3)][1] + grow.boots[i,-(1:3)][3],
+      #   trt %in% 'irrigated' ~ grow.boots[i,-(1:3)][1] + grow.boots[i,-(1:3)][4]
+      # )
+    )
+  
+  # 2: Growth model slope
+  
+  this.boot[[2]] = bootstrap.backbone %>%
+    # Predicted survival
+    mutate(
+      pred.surv = predict(
+        newdata = .,
+        object = s_s, type = 'response',
+        newparams = surv.boots[i,-(1:2)],
+        re.form = ~ 0, allow.new.levels = TRUE
+      )
+    ) %>%
+    # Predicted growth
+    mutate(
+      pred.grow.mean = predict(
+        newdata = .,
+        object = g_st.ty, type = 'response',
+        newparams = grow.boots[i,-(1:2)] %>%
+          (function(x) {
+            x[2] <- x[2] + delta
+            return(x)
+          }),
+        re.form = ~ 0, allow.new.levels = TRUE
+      )
+    ) %>%
+    mutate(p.grow.size = 0.1 * dnorm(size.cur, pred.grow.mean, sd = sqrt(exp(grow.boots$betad[i])))) %>%
+    # Combine all together to get overall size distribution in next time step
+    mutate(p.size.cur = pred.surv * p.grow.size) %>%
+    select(-c(pred.surv, pred.grow.mean, p.grow.size)) %>%
+    mutate(
+      perturb.param = 'grow.slope'
+      # orig.par.val = case_when(
+      #   trt %in% 'control' ~ grow.boots[i,-(1:3)][2],
+      #   trt %in% 'drought' ~ grow.boots[i,-(1:3)][2] + grow.boots[i,-(1:3)][5],
+      #   trt %in% 'irrigated' ~ grow.boots[i,-(1:3)][2] + grow.boots[i,-(1:3)][6]
+      # )
+    )
+  
+  gs.pert.boot[[i]] = do.call(rbind, this.boot) %>% 
+    mutate(boot = paste0('b', i))
+  
+  print(i)
+  
+}
+
+do.call(rbind, gs.pert.boot) %>%
+  pivot_wider(names_from = boot, values_from = p.size.cur) %>%
+  write.csv(
+    '03_construct_kernels/out/deterministic_growsurv_perturb_bootstraps.csv',
+    row.names = FALSE, na = ''
+  )
+
+
+# Export perturbed parameters
+cbind(
+  boot = 1:n.straps,
+  grow.int_control = unlist(grow.boots[,-(1:2)][1]),
+  grow.int_drought = unlist(grow.boots[,-(1:2)][1] + grow.boots[,-(1:2)][3]),
+  grow.int_irrigated = unlist(grow.boots[,-(1:2)][1] + grow.boots[,-(1:2)][4]),
+  grow.slope_control = unlist(grow.boots[,-(1:2)][2]),
+  grow.slope_drought = unlist(grow.boots[,-(1:2)][2] + grow.boots[,-(1:2)][5]),
+  grow.slope_irrigated = unlist(grow.boots[,-(1:2)][2] + grow.boots[,-(1:2)][6])
+) %>%
+  write.csv(
+    file = '03_construct_kernels/out/reprod_bootstrapped_perturbed_params.scv',
     row.names = FALSE, na = ''
   )
