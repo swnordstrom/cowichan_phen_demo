916dcc4c6b2a2c33df118d66023cf5cd7a959147
swnordstrom <swnordstrom@email.wm.edu>
2025-01-14 10:30:21 -0800

    add ltre kernel to growsurv-phen kernel script

--- 03_construct_kernels/deterministic_growthsurv_kernel_phen.R
+++ 03_construct_kernels/deterministic_growthsurv_kernel_phen.R
@@ -82,8 +82,8 @@ demo.grow = demo.surv.sizes %>%
 demo.surv.sizes = demo.surv.sizes %>% filter(surv.year < 2024)
 
 # Get phenology dataset
-
-phen = all.data %>%
+# this is for merging in with growth dataset, so relevant measure is by plant (not by umbel)
+phen.by.plant.for.growth = all.data %>%
   # Give me plants that are in phenology
   filter(in.phen) %>%
   # Extract the bud dates on file
@@ -98,261 +98,30 @@ phen = all.data %>%
   ungroup()
 
 demo.grow = merge(
-  demo.grow, phen, 
+  demo.grow, phen.by.plant.for.growth, 
   by.x = c('prev.year', 'plantid'), by.y = c('Year', 'plantid'),
   all.x = TRUE, all.y = FALSE
 ) %>%
   mutate(phen.mean.c = phen.mean - round(mean(phen.mean, na.rm = TRUE)))
 
-### ----- MODEL SELECTION -----
+# Phenology for mean bud date by phenology
+# i.e., for umbel
+# This is used for getting dates for LTRE
+# Umbel-level budding phenology data
 
-# # ------------------------------------------------
-# # Fit survival models
-# 
-# # First question: do we need size and/or year
-# # (I suspect we will)
-# 
-# s_0 = glmmTMB(
-#   formula = surv ~ (1 | Plot),
-#   family = 'binomial',
-#   data = demo.surv.sizes
-# )
-# 
-# s_y = glmmTMB(
-#   formula = surv ~ (1 | surv.year) + (1 | Plot),
-#   family = 'binomial',
-#   data = demo.surv.sizes
-# )
-# 
-# s_s = glmmTMB(
-#   formula = surv ~ size.prev + (1 | Plot),
-#   family = 'binomial',
-#   data = demo.surv.sizes
-# )
-# 
-# s_s_y = glmmTMB(
-#   formula = surv ~ size.prev + (1 | surv.year) + (1 | Plot),
-#   family = 'binomial',
-#   data = demo.surv.sizes
-# )
-# 
-# AIC(s_0, s_y, s_s, s_s_y) %>% mutate(daic = round(AIC - min(AIC), 2))
-# 
-# # Interesting! No support for year effects.
-# 
-# summary(s_s)
-# # Increasing size increases odds of survival, unsurprisingly!
-# 
-# # Models to test:
-# # - Treatment effect
-# # - Size-treatment interaciton
-# 
-# s_t = glmmTMB(
-#   formula = surv ~ size.prev + trt + (1 | Plot),
-#   family = 'binomial',
-#   data = demo.surv.sizes
-# )
-# 
-# s_ts = glmmTMB(
-#   formula = surv ~ size.prev * trt + (1 | Plot),
-#   family = 'binomial',
-#   data = demo.surv.sizes
-# )
-# 
-# AIC(s_s, s_t, s_ts) %>% mutate(daic = round(AIC - min(AIC), 2))
-# # No treatment effects.
-# 
-# # Plot of this model:
-# surv.preds = data.frame(size.prev = (5:60) / 10) %>%
-#   mutate(pred = predict(s_s, re.form = ~ 0, newdata = ., allow.new.levels = TRUE, type = 'response'))
-# 
-# head(surv.preds)
-# 
-# demo.surv.sizes %>%
-#   mutate(surv = as.numeric(surv)) %>%
-#   ggplot(aes(x = size.prev, y = surv)) +
-#   geom_point(position = position_jitter(height = 0.2), alpha = 0.1, size = 3) +
-#   geom_line(data = surv.preds, aes(y = pred))
-# 
-# # # Random effects:
-# 
-# # Plot level effects:
-# data.frame(ranef(s_s_y)$cond$Plot) %>%
-#   mutate(Plot = row.names(.)) %>%
-#   rename(incp = `X.Intercept.`) %>%
-#   merge(distinct(demo.surv.sizes, Plot, trt)) %>%
-#   mutate(Plot = factor(Plot)) %>%
-#   ggplot(aes(x = Plot, y = incp, colour = trt)) +
-#   geom_point(size = 4) +
-#   scale_colour_manual(values = c('black', 'red', 'blue'))
-# # Definitely not seeing a clear pattern here
-# 
-# # Maybe slightly higher intercepts for irrigated but I trust the models
-# # suggesting no treatment effect
-# 
-# 
-# # ------------------------------------------------
-# # Fit growth models
-# 
-# # Test for year effects
-# 
-# g_0 = glmmTMB(
-#   size.cur ~ size.prev + (1 | Plot / plantid),
-#   data = demo.grow
-# )
-# 
-# g_y = glmmTMB(
-#   size.cur ~ size.prev + (1 | prev.year) + (1 | Plot / plantid),
-#   data = demo.grow
-# )
-# 
-# AIC(g_y, g_0)
-# # Yep. Support for a year effect.
-# 
-# # Treatment effect
-# 
-# g_t = glmmTMB(
-#   size.cur ~ size.prev + trt + (1 | prev.year) + (1 | Plot / plantid),
-#   data = demo.grow
-# )
-# 
-# AIC(g_t, g_y)
-# # Wow... no treatment effect.
-# 
-# # There are a couple of other things to try:
-# # - treatment-year
-# # - treatment-size
-# # - size-year
-# 
-# g_ty = glmmTMB(
-#   size.cur ~ size.prev + trt + (1 | prev.year) + (1 | prev.year:trt) + (1 | Plot / plantid),
-#   data = demo.grow
-# )
-# 
-# g_st.ty = glmmTMB(
-#   size.cur ~ size.prev + size.prev * trt + (1 | prev.year) + (1 | prev.year:trt) + (1 | Plot / plantid),
-#   data = demo.grow
-# )
-# 
-# g_st = glmmTMB(
-#   size.cur ~ size.prev * trt + (1 | prev.year) + (1 | Plot / plantid),
-#   data = demo.grow
-# )
-# 
-# g_sy = glmmTMB(
-#   size.cur ~ size.prev + (size.prev | prev.year) + (1 | Plot / plantid),
-#   data = demo.grow
-# )
-# 
-# AIC(g_y, g_t, g_ty, g_st.ty, g_st, g_sy) %>% mutate(daic = round(AIC - min(AIC), 2))
-# # Yep - as before, overwhelming support for the year-treatment interaction
-# 
-# summary(g_st.ty)
-# summary(g_ty)
-# # These feel like *very* weak effects on average...
-# # (We also have slight advantages to both drought and irrigation...)
-# 
-# # glmmTMB won't let me generate predictions with half-specified random effects
-# data.frame(ranef(g_st.ty)$cond$`prev.year:trt`) %>%
-#   rename(intcp = `X.Intercept.`) %>%
-#   mutate(year.trt = row.names(.)) %>%
-#   separate(year.trt, into = c('year', 'trt'), sep = ':') %>%
-#   ggplot(aes(x = year, y = intcp, colour = trt, group = trt)) +
-#   geom_line() +
-#   scale_colour_manual(values = c('black', 'red', 'blue'))
-# # Hmm... yep pretty noisy  
-# # Although if the mean effect of drought is +.12... that's actually kinda big
-# # compared to the magnitude of fluctuations?
-# 
-# # What would results look like if I made predictions for the median plant+plot?
-# abs(ranef(g_st.ty)$cond$Plot) %>% arrange(`(Intercept)`) # median plot is 13, eff is +.0222
-# ranef(g_st.ty)$cond$`plantid:Plot` %>% 
-#   mutate(pla = row.names(.)) %>% 
-#   filter(grepl('\\_13\\:', pla)) %>% 
-#   mutate(eff = `(Intercept)` + .0222) %>% 
-#   arrange(abs(eff))
-# # seems like median plant is our old friend 3989_13:13
-# 
-# expand.grid(
-#   size.prev = (5:60)/10,
-#   prev.year = factor(2017:2023),
-#   trt = c('control', 'drought', 'irrigated')
-# ) %>%
-#   mutate(Plot = 13, plantid = '3989_13') %>%
-#   mutate(pp = predict(g_st.ty, newdata = ., re.form = NULL)) %>%
-#   ggplot(aes(x = size.prev, y = pp, colour = trt, group = trt)) +
-#   geom_segment(aes(x = 0.5, y = 0.5, xend = 6, yend = 6), linetype = 2, colour = 'gray77') +
-#   geom_line() +
-#   scale_colour_manual(values = c('black', 'red', 'blue')) +
-#   facet_wrap(~ prev.year)
-# # Usually, but not always, control performing below drought...
-# # (It would be cool to iterate individuals through the years...)
-# 
-# # Compare predictions from our two different models
-# expand.grid(
-#   size.prev = (5:60)/10,
-#   trt = c('control', 'drought', 'irrigated')
-# ) %>%
-#   mutate(
-#     pred.yrf = predict(g_st.ty, newdata = ., re.form = ~ 0, allow.new.levels = TRUE),
-#     pred.nrf = predict(g_st, newdata = ., re.form = ~ 0, allow.new.levels = TRUE)
-#   ) %>%
-#   ggplot(aes(x = pred.nrf, y = pred.yrf, colour = trt)) +
-#   geom_segment(aes(x = 2, y = 2, xend = 4.5, yend = 4.5), linetype = 2, colour = 'gray77') +
-#   geom_point(size = 3, alpha = 0.25) +
-#   labs(x = 'non-year varying', y = 'year-varying') +
-#   scale_colour_manual(values = c('black', 'red', 'blue'))
-# # above diagonal: greater predicted growth in year-varying model
-# # below diagonal: greater predicted growth in non-year varying model
-# # there's the *least* change in the control treatment...
-# 
-# expand.grid(
-#   size.prev = (5:60)/10,
-#   trt = c('control', 'drought', 'irrigated')
-# ) %>%
-#   mutate(
-#     pred.yrf = predict(g_st.ty, newdata = ., re.form = ~ 0, allow.new.levels = TRUE),
-#     pred.nrf = predict(g_st, newdata = ., re.form = ~ 0, allow.new.levels = TRUE)
-#   ) %>%
-#   pivot_longer(cols = c(pred.yrf, pred.nrf), names_to = 'model', values_to = 'pred') %>%
-#   ggplot(aes(x = size.prev, y = pred, group = interaction(trt, model), colour = trt)) +
-#   geom_segment(aes(x = .5, y = .5, xend = 6, yend = 6), linetype = 2, colour = 'gray77') +
-#   geom_line(aes(linetype = model)) +
-#   labs(x = 'non-year varying', y = 'year-varying') +
-#   scale_colour_manual(values = c('black', 'red', 'blue'))
-# # The model with the year-varying effects has less shrinkage for large plants, less growth for small plants
-# 
-# # I just don't trust these random effects
-# AIC(g_y, g_st, g_sy) %>% mutate(daic = round(AIC - min(AIC), 2))
-# 
-# # Out of these, g_st appears to be best supported
-# summary(g_st.ty)
-# 
-# expand.grid(trt = c('control', 'drought', 'irrigated'), size.prev = (5:60)/10) %>%
-#   mutate(size.cur = predict(g_st.ty, re.form = ~ 0, newdata = ., allow.new.levels = TRUE)) %>%
-#   ggplot(aes(x = size.prev, y = size.cur, colour = trt, group = trt)) +
-#   geom_segment(aes(x = .5, y = .5, xend = 6, yend = 6), linetype = 2, linewidth = 0.25) +
-#   geom_point(data = demo.grow, size = 3, alpha = 0.1) +
-#   geom_line() +
-#   scale_colour_manual(values = c('black', 'red', 'blue')) +
-#   facet_wrap(~ trt)
-# # Hmm...
-# 
-# # Look at residuals
-# demo.grow %>%
-#   mutate(resids = residuals(g_st.ty)) %>%
-#   ggplot(aes(x = size.prev, y = resids, colour = trt)) +
-#   geom_segment(aes(x = .5, xend = 6, y = 0, yend = 0), colour = 'gray', linetype = 2, linewidth = 0.5) +
-#   scale_colour_manual(values = c('black' ,'red', 'blue')) +
-#   geom_point(alpha = 0.1, size = 3)
-# # Eh... a few outliers for medium-large plants...
-# 
-# demo.grow %>%
-#   mutate(resids = residuals(g_st)) %>%
-#   ggplot(aes(x = prev.year, y = resids)) +
-#   geom_segment(aes(x = 2017, xend = 2023, y = 0, yend = 0), linetype = 2, linewidth = 0.5) +
-#   geom_point(position = position_jitter(width = 0.4), alpha = 0.05, size = 3)
-# # A few negative outliers in 2022, 2023 but nothing awful.
+phen.by.umbel.for.ltre = all.data %>% 
+  filter(in.phen) %>%
+  # Split out the bud dates for bud date models; the most umbels seen in a
+  # plant is 12, so use separate() to kick these out and then pivot_long to get
+  # one row per umbel
+  # (first - need to get one row per plant - do a distinct())
+  distinct(Year, plantid, .keep_all = TRUE) %>%
+  separate_wider_delim(phen.julis, names = paste0('uu', 1:12), delim = ';', too_few = 'align_start') %>%
+  pivot_longer(starts_with('uu'), names_to = 'umbel.number', values_to = 'phen.julian') %>%
+  filter(!is.na(phen.julian)) %>%
+  mutate(phen.julian = as.numeric(gsub('\\s', '', phen.julian))) %>%
+  mutate(Year = factor(Year))
+
 
 # ------------------------------------------------
 # Construct kernels
@@ -378,6 +147,12 @@ g_phen = glmmTMB(
   data = demo.grow %>% filter(!is.na(phen.mean.c))
 )
 
+# Phen (umbel, treatment effects) model
+d_t = glmmTMB(
+  phen.julian ~ trt + Year + (1 | Plot / plantid),
+  data = phen.by.umbel.for.ltre
+)
+
 # --- Extract parameters needed
 
 # Residual variance in growth models
@@ -391,7 +166,7 @@ grow.surv.kernel = expand.grid(
   size.prev = (5:60)/10,
   size.cur = (5:60)/10,
   trt = c('control', 'drought', 'irrigated'),
-  prev.year = 2021:2023,
+  year = 2021:2023,
   phen.mean.c = -28:28
 )
 
@@ -406,25 +181,32 @@ grow.surv.kernel = grow.surv.kernel %>%
   ) %>%
   # Predicted growth
   mutate(
+    # Model without phenology
     pred.grow.mean = predict(
       newdata = .,
       object = g_st.ty, type = 'response',
       re.form = ~ 0, allow.new.levels = TRUE
-    ),
+    )
+  ) %>%
+  # Model with phenology
+  # Need to change name of year column to get annual predictions
+  rename(prev.year = year) %>%
+  mutate(
     phen.grow.mean = predict(
       newdata = .,
       object = g_phen, type = 'response',
       re.form = ~ 0, allow.new.levels = TRUE
     )
   ) %>%
+  # Take the average of the growth kernel across years
   group_by(size.prev, size.cur, trt, phen.mean.c, pred.surv, pred.grow.mean) %>%
   summarise(phen.grow.mean = mean(phen.grow.mean)) %>%
+  ungroup() %>%
   # Predicted distribution of sizes in next time step
   mutate(
     pv.grow.size = 0.1 * dnorm(size.cur, mean = pred.grow.mean, sd = gv.sd),
     pf.grow.size = 0.1 * dnorm(size.cur, mean = phen.grow.mean, sd = gf.sd)
-  ) %>%
-  ungroup()
+  )
 
 grow.surv.kernel %>%
   filter(phen.mean.c %in% c(-28, 28)) %>%
@@ -449,9 +231,98 @@ grow.surv.kernel %>%
 
 write.csv(
   grow.surv.kernel %>%
-    mutate(phen = phen.mean.c + round(mean(phen$phen.mean))) %>%
+    mutate(phen = phen.mean.c + round(mean(phen.by.plant.for.growth$phen.mean))) %>%
     select(-c(phen.mean.c, phen.grow.mean, pred.grow.mean)),
   file = '03_construct_kernels/out/deterministic_growsurv_kernel_phen.csv',
+  row.names = FALSE
+)
+
+
+# --- Lambda estimates for LTRE
+
+# Mean bud date for each treatment
+trt.mean.buddates = expand.grid(trt = c('control', 'drought', 'irrigated'), Year = factor(2021:2024)) %>%
+  mutate(
+    mean.bud = predict(
+      d_t, re.form = ~ 0, allow.new.levels = TRUE,
+      newdata = expand.grid(trt = c('control', 'drought', 'irrigated'), Year = factor(2021:2024))
+    )
+  ) %>%
+  group_by(trt) %>%
+  summarise(mean.phen = mean(mean.bud))
+
+# LTRE backbone (combinations of treatment used to estimate vital rate and
+# treatment used for phenology)
+ltre.backbone = expand.grid(
+  size.prev = (5:60)/10,
+  size.cur  = (5:60)/10,
+  year = 2021:2023,
+  # This column will be used for manipulating the phenology date and the vital
+  # rate estimation
+  trt.phen.idx = 1:7
+) %>%
+  merge(
+    data.frame(
+      trt.phen.idx = 1:7,
+      # Treatment associated with the buddates used for umbel success + seeds
+      trt.phen = c('drought', 'control', 'irrigated', 'control', 'drought', 'irrigated', 'control'),
+      # Treatment associated with direct treatment effects on vital rates
+      trt.rate = c('drought', 'drought', 'irrigated', 'irrigated', 'control', 'control', 'control')
+    )
+  ) %>%
+  # Merge with estimated mean bud date per treatment
+  merge(trt.mean.buddates, by.x = 'trt.phen', by.y = 'trt') %>%
+  # Rename trt column so it is used in models
+  rename(trt = trt.rate) %>% 
+  # center the phenology column and rename the `trt` column so it can be used in
+  # vital rate estimates
+  mutate(phen.mean.c = mean.phen - round(mean(phen.by.plant.for.growth$phen.mean)))
+
+# Get kernel
+ltre.kernel = ltre.backbone %>%
+  # Predicted survival
+  mutate(
+   pred.surv = predict(
+    newdata = .,
+    object = s_s, type = 'response',
+    re.form = ~ 0, allow.new.levels = TRUE
+  )
+) %>%
+  # Predicted growth
+  mutate(
+    # Model without phenology
+    pred.grow.mean = predict(
+      newdata = .,
+      object = g_st.ty, type = 'response',
+      re.form = ~ 0, allow.new.levels = TRUE
+    )
+  ) %>%
+  # Model with phenology
+  # Need to change name of year column to get annual predictions
+  rename(prev.year = year) %>%
+  mutate(
+    phen.grow.mean = predict(
+      newdata = .,
+      object = g_phen, type = 'response',
+      re.form = ~ 0, allow.new.levels = TRUE
+    )
+  ) %>%
+  # Take the average of the growth kernel across years
+  group_by(size.prev, size.cur, trt, trt.phen, phen.mean.c, pred.surv, pred.grow.mean) %>%
+  summarise(phen.grow.mean = mean(phen.grow.mean)) %>%
+  ungroup() %>%
+  # Predicted distribution of sizes in next time step
+  mutate(
+    pv.grow.size = 0.1 * dnorm(size.cur, mean = pred.grow.mean, sd = gv.sd),
+    pf.grow.size = 0.1 * dnorm(size.cur, mean = phen.grow.mean, sd = gf.sd)
+  )
+
+# Export
+write.csv(
+  ltre.kernel %>%
+    mutate(phen = phen.mean.c + round(mean(phen.by.plant.for.growth$phen.mean))) %>%
+    select(-c(phen.mean.c, phen.grow.mean, pred.grow.mean)),
+  file = '03_construct_kernels/out/deterministic_growsurv_kernel_phen_ltre.csv',
   row.names = FALSE
 )
 
@@ -465,7 +336,7 @@ write.csv(
 # - Growth model:
 #   - Intercept
 #   - Slope (size-dependence)
-#   - Residual (standard deviation term)
+# - 
 
 # Perturbation amount
 delta = 0.0001
