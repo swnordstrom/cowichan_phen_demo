9fc0966fc566c25fe5fe9715aef4747290d57402
swnordstrom <swnordstrom@email.wm.edu>
2025-01-24 17:35:21 -0800

    rename nophen growthsurv kernel bootstrap script

--- 03_construct_kernels/deterministic_growthsurv_bootstrapping_nophen.R
+++ 03_construct_kernels/deterministic_growthsurv_bootstrapping_nophen.R
@@ -1,5 +1,6 @@
 # Script for generating bootstrapped survival-growth subkernels, including
 # perturbations
+# This script includes the growth-phenology trade-off
 # Outputs:
 # - 100 bootstrapped growth+surv sub-kernels
 # - 100 bootstrapped perturbed growth+surv sub-kernels
@@ -15,72 +16,13 @@ library(glmmTMB)
 
 rm(list = ls())
 
-# # Data preparation 
-all.data = merge(
-  x = read.csv('01_data_cleaning/out/demo_phen_seed_2016-2024_final.csv'),
-  y = read.csv('00_raw_data/plot_treatments.csv'),
-  by.x = 'Plot', by.y = 'plot'
-)
+source('03_construct_kernels/prepare_demo_data_growsurv.R')
 
-nrow(all.data)
-head(all.data)
-
-all.demo = all.data %>% 
-  filter(in.demo) %>%
-  distinct(plantid, Year, .keep_all = TRUE)
-
-nrow(all.demo)
-
-# Get survival dataset
-
-# Survival dataset:
-# (Two versions: a size-dependent one and a size-independent one
-# almost surely we will use the size-dependent one for analysis)
-
-demo.surv = merge(
-  # Demo in time step t+1
-  x = all.demo %>% 
-    mutate(prev.year = Year - 1) %>%
-    rename(surv.year = Year) %>%
-    select(Plot, plantid, surv.year, prev.year, No.leaves, Leaf.length, surv, trt),
-  # Demo in time step t
-  y = all.demo %>%
-    # we are *only* interested in plants alive in time step t
-    filter(surv) %>%
-    # Select relevant columns
-    select(Plot, plantid, Year, No.leaves, Leaf.length, trt),
-  by.x = c('Plot', 'plantid', 'prev.year', 'trt'),
-  by.y = c('Plot', 'plantid', 'Year', 'trt'),
-  suffixes = c('', '.pre'),
-  all.x = FALSE, all.y = FALSE
-)
-
-head(demo.surv)
-# should be less than 1
-table(demo.surv$surv, useNA = 'always')
-# good
-
-# Want to get a column where we can estimate sizes
-demo.surv.sizes = demo.surv %>% 
-  filter(
-    !is.na(Leaf.length.pre) & !is.na(No.leaves.pre) &
-      Leaf.length.pre > 0 & No.leaves.pre > 0
-  ) %>%
-  # Get rid of 2016 records because the sizes are not reliable
-  filter(prev.year > 2016) %>%
-  # Add size columns
-  mutate(size.prev = log(No.leaves.pre * Leaf.length.pre))
-
-nrow(demo.surv.sizes)
-table(demo.surv.sizes$surv, useNA = 'always')
-
-# Subset for growth estimation
-demo.grow = demo.surv.sizes %>% 
-  filter(surv, !is.na(Leaf.length) & !is.na(No.leaves) & Leaf.length > 0 & No.leaves > 0) %>%
-  mutate(size.cur = log(Leaf.length * No.leaves))
-
-# Finally: subset surv dataset to not include 2023-2024 surv
-demo.surv.sizes = demo.surv.sizes %>% filter(surv.year < 2024)
+# Read in bootstrapped phen estimates
+# done in script `phenology_bootstrapped_estimares.R`
+phen.boot.trt = read.csv('03_construct_kernels/out/phenology_bootstrapped_means.csv') %>%
+  # Convert to long data frame
+  pivot_longer(-boot, names_to = 'trt', values_to = 'mean.phen')
 
 # --- Original models -------------------------------------------------------
 # Need to run original models 
@@ -98,12 +40,18 @@ g_st.ty = glmmTMB(
   data = demo.grow
 )
 
+# Growth-phen model
+g_phen = glmmTMB(
+  size.cur ~ size.prev * prev.year + trt * prev.year + phen.c + (1 | Plot / plantid),
+  data = demo.grow %>% filter(!is.na(phen.c))
+)
+
 # --- Boots ---------------------------------------------------------
 
 set.seed(11225)
 
 # Number of bootstraps
-n.straps = 100
+n.straps = 20
 
 # Generate bootstrapped estimates
 
@@ -244,7 +192,7 @@ for (i in 1:n.straps) {
 do.call(rbind, kernel.list) %>%
   pivot_wider(names_from = boot, values_from = p.size.cur) %>%
   write.csv(
-    '03_construct_kernels/out/deterministic_growsurv_bootstrap.csv',
+    '03_construct_kernels/out/deterministic_growsurv_bootstrap250.csv',
     row.names = FALSE, na = ''
   )
 
