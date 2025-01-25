582faa341e38e847f453cba91b8e24a368819053
swnordstrom <swnordstrom@email.wm.edu>
2025-01-24 17:35:45 -0800

    fix read-ins and change column saving in growthsurv kernel script

--- 03_construct_kernels/deterministic_growthsurv_kernel_phen.R
+++ 03_construct_kernels/deterministic_growthsurv_kernel_phen.R
@@ -15,107 +15,7 @@ library(glmmTMB)
 
 rm(list = ls())
 
-# Read in demo data and merge with treatment info
-all.data = merge(
-  x = read.csv('01_data_cleaning/out/demo_phen_seed_2016-2024_final.csv'),
-  y = read.csv('00_raw_data/plot_treatments.csv'),
-  by.x = 'Plot', by.y = 'plot'
-)
-
-nrow(all.data)
-head(all.data)
-
-# Read in phenology means
-phen.treatment.means = read.csv('03_construct_kernels/phen_treatment_means.csv')
-# Mean that will be used for centering
-phen.ctrl.mean = phen.treatment.means$mean.phen[phen.treatment.means$trt %in% 'control']
-
-# We're interested only in plants that are in demo for this analysis
-all.demo = all.data %>% 
-  filter(in.demo) %>%
-  distinct(plantid, Year, .keep_all = TRUE)
-
-nrow(all.demo)
-
-# Get survival dataset
-
-# Survival dataset:
-# (Two versions: a size-dependent one and a size-independent one
-# almost surely we will use the size-dependent one for analysis)
-
-demo.surv = merge(
-  # Demo in time step t+1
-  x = all.demo %>% 
-    mutate(prev.year = Year - 1) %>%
-    rename(surv.year = Year) %>%
-    select(Plot, plantid, surv.year, prev.year, No.leaves, Leaf.length, surv, trt),
-  # Demo in time step t
-  y = all.demo %>%
-    # we are *only* interested in plants alive in time step t
-    filter(surv) %>%
-    # Select relevant columns
-    select(Plot, plantid, Year, No.leaves, Leaf.length, trt),
-  by.x = c('Plot', 'plantid', 'prev.year', 'trt'),
-  by.y = c('Plot', 'plantid', 'Year', 'trt'),
-  suffixes = c('', '.pre'),
-  all.x = FALSE, all.y = FALSE
-) %>%
-  # Change years to factors
-  mutate(across(contains('year'), as.factor))
-
-head(demo.surv)
-# should be less than 1
-table(demo.surv$surv, useNA = 'always')
-# good
-
-# Want to get a column where we can estimate sizes
-demo.surv.sizes = demo.surv %>% 
-  filter(
-    !is.na(Leaf.length.pre) & !is.na(No.leaves.pre) &
-      Leaf.length.pre > 0 & No.leaves.pre > 0
-  ) %>%
-  # Get rid of 2016 records because the sizes are not reliable
-  filter(!(prev.year %in% 2016)) %>%
-  # Add size columns
-  mutate(size.prev = log(No.leaves.pre * Leaf.length.pre))
-
-nrow(demo.surv.sizes)
-table(demo.surv.sizes$surv, useNA = 'always')
-
-# Subset for growth estimation
-demo.grow = demo.surv.sizes %>% 
-  filter(surv, !is.na(Leaf.length) & !is.na(No.leaves) & Leaf.length > 0 & No.leaves > 0) %>%
-  mutate(size.cur = log(Leaf.length * No.leaves))
-
-# Finally: subset surv dataset to not include 2023-2024 surv
-demo.surv.sizes = demo.surv.sizes %>% filter(!(surv.year %in% 2024))
-
-# Get phenology dataset
-# this is for merging in with growth dataset, so relevant measure is by plant (not by umbel)
-phen.by.plant.for.growth = all.data %>%
-  # Give me plants that are in phenology
-  filter(in.phen) %>%
-  # Extract the bud dates on file
-  separate_wider_delim(phen.julis, names = paste0('uu', 1:12), delim = ';', too_few = 'align_start') %>%
-  pivot_longer(starts_with('uu'), names_to = 'umbel.number', values_to = 'phen.julian') %>%
-  filter(!is.na(phen.julian)) %>%
-  # convert to julian date
-  mutate(phen.julian = as.numeric(gsub('\\s', '', phen.julian))) %>%
-  # give me the mean bud date for each plant
-  group_by(plantid, Year) %>%
-  summarise(phen.mean = mean(phen.julian)) %>%
-  ungroup() %>%
-  # Change year to factor
-  mutate(Year = as.factor(Year))
-
-demo.grow = merge(
-  demo.grow, phen.by.plant.for.growth, 
-  by.x = c('prev.year', 'plantid'), by.y = c('Year', 'plantid'),
-  all.x = TRUE, all.y = FALSE
-) %>%
-  # Center mean around control
-  mutate(phen.c = phen.mean - phen.ctrl.mean)
-
+source('03_construct_kernels/prepare_demo_data_growsurv.R')
 
 # ------------------------------------------------
 # Construct kernels
@@ -215,8 +115,8 @@ grow.surv.kernel %>%
 
 write.csv(
   grow.surv.kernel %>%
-    mutate(phen = phen.c + phen.ctrl.mean) %>%
-    select(-c(phen.c, phen.grow.mean, pred.grow.mean)),
+    # mutate(phen = phen.c + phen.ctrl.mean) %>%
+    select(-c(phen.grow.mean, pred.grow.mean)),
   file = '03_construct_kernels/out/deterministic_growsurv_kernel_phen.csv',
   row.names = FALSE
 )
@@ -234,16 +134,10 @@ ltre.backbone = expand.grid(
   # rate estimation
   trt.phen.idx = 1:7
 ) %>%
-  merge(
-    data.frame(
-      trt.phen.idx = 1:7,
-      # Treatment associated with the buddates used for umbel success + seeds
-      trt.phen = c('drought', 'control', 'irrigated', 'control', 'drought', 'irrigated', 'control'),
-      # Treatment associated with direct treatment effects on vital rates
-      trt.rate = c('drought', 'drought', 'irrigated', 'irrigated', 'control', 'control', 'control')
-    )
-  ) %>%
+  # Read in combinations of treatment and phenology for LTRE desigm
+  merge(read.csv('03_construct_kernels/ltre_treatment_key.csv')) %>%
   # Merge with estimated mean bud date per treatment
+  # 'trt.phen' column is the treatment for which mean phenology is used
   merge(phen.treatment.means, by.x = 'trt.phen', by.y = 'trt') %>%
   # Rename trt column so it is used in models
   rename(trt = trt.rate) %>% 
@@ -285,8 +179,9 @@ ltre.kernel = ltre.backbone %>%
 # Export
 write.csv(
   ltre.kernel %>%
-    mutate(phen = phen.c + phen.ctrl.mean) %>%
-    select(-c(phen.c, mean.phen, phen.grow.mean, pred.grow.mean, trt.phen.idx)),
+    select(size.prev, size.cur, trt.phen.idx, pred.surv, pv.grow.size, pf.grow.size),
+    # mutate(phen = phen.c + phen.ctrl.mean) %>%
+    # select(-c(phen.c, mean.phen, phen.grow.mean, pred.grow.mean, trt.phen.idx)),
   file = '03_construct_kernels/out/deterministic_growsurv_kernel_phen_ltre.csv',
   row.names = FALSE
 )
@@ -448,14 +343,14 @@ outputs[[3]] = ltre.backbone %>%
 
 
 # Bind them all together
-outputs.all = do.call(rbind, outputs) %>%
+outputs.all = do.call(rbind, outputs) # %>%
   # De-center phenology
-  mutate(phen = phen.c + phen.ctrl.mean)
+  # mutate(phen = phen.c + phen.ctrl.mean)
 
 write.csv(
   outputs.all %>% 
     select(
-      size.prev, size.cur, trt, trt.phen, phen, 
+      size.prev, size.cur, trt.phen.idx, 
       pred.surv, pv.grow.size, pf.grow.size, perturb.param, orig.par.val
     ),
   file = '03_construct_kernels/out/deterministic_grow_coef_perturbation_phen.csv',
