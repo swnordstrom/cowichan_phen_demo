bd7438c70370b0d9b2ed0cae40a08ba72b689541
swnordstrom <swnordstrom@email.wm.edu>
2025-01-24 17:38:25 -0800

    update LTRE scirpt to include growth-phen trade-off and trt-phen indexing change

--- 04_analysis/deterministic_ltre.R
+++ 04_analysis/deterministic_ltre.R
@@ -44,41 +44,52 @@ fr.obsv.pert = read.csv('03_construct_kernels/out/deterministic_repr_coef_pertur
 
 # --- Read in kernels from bootstrapped resampling
 
-gs.boot = read.csv('03_construct_kernels/out/deterministic_growsurv_bootstrap250.csv')
-fr.boot = read.csv('03_construct_kernels/out/deterministic_reprod_bootstrap_ltre_250.csv')
+gs.boot = read.csv('03_construct_kernels/out/deterministic_growsurv_bootstrap_ltre.csv')
+fr.boot = read.csv('03_construct_kernels/out/deterministic_reprod_bootstrap_ltre.csv') %>%
+  mutate(boot = gsub('b', '', boot))
 
 
 # --- Read in perturbed kernels from bootstrapped sampling
 
-gs.boot.pert = read.csv('03_construct_kernels/out/deterministic_growsurv_perturb_bootstraps250.csv')
-fr.boot.pert = read.csv('03_construct_kernels/out/deterministic_reprod_perturb_bootstraps_250.csv')
+gs.boot.pert = read.csv('03_construct_kernels/out/deterministic_growsurv_perturb_bootstraps.csv')
+fr.boot.pert = read.csv('03_construct_kernels/out/deterministic_reprod_perturb_bootstraps.csv')
 
 
 # --- Read in parameters used in bootstrapping
 # (these give the differences in beta in the LTRE)
 
-gs.pert.pars = read.csv('03_construct_kernels/out/growsurv_bootstrapped_perturbed_params250.csv') %>%
-  pivot_longer(-boot, names_to = 'rate_trt', values_to = 'parval') %>%
-  separate(rate_trt, into = c('rate', 'trt'), sep = '_')
+gs.pert.pars = read.csv('03_construct_kernels/out/growsurv_bootstrapped_perturbed_params.csv') # %>%
+  # pivot_longer(-boot, names_to = 'rate_trt', values_to = 'parval') %>%
+  # separate(rate_trt, into = c('rate', 'trt'), sep = '_')
 
-fr.pert.pars = read.csv('03_construct_kernels/out/reprod_bootstrapped_perturbed_params250.csv') 
+fr.pert.pars = read.csv('03_construct_kernels/out/reprod_bootstrapped_perturbed_params.csv') 
 # Do this in two steps because this file also contains the phen dates used in the bootstarp
 
 # Dates used in phen bootstrapping
-phen.boots = fr.pert.pars %>%
-  select(c(boot, contains('phen'))) %>%
-  pivot_longer(-boot, names_to = 'trt.phen', values_to = 'phen') %>%
-  mutate(
-    trt.phen = gsub('phen\\_', '', trt.phen),
-    phen = as.Date(as.numeric(phen), format = '%b-%d')
-  )
+phen.boots = read.csv('03_construct_kernels/out/phenology_bootstrapped_means.csv') # %>%
+  # select(c(boot, contains('phen'))) %>%
+  # pivot_longer(-boot, names_to = 'trt.phen', values_to = 'phen') %>%
+  # mutate(
+  #   trt.phen = gsub('phen\\_', '', trt.phen),
+  #   phen = as.Date(as.numeric(phen), format = '%b-%d')
+  # )
 
-# Reproductive bootstrapped parameters
-fr.pert.pars = fr.pert.pars %>%
-  select(-contains('phen')) %>%
-  pivot_longer(-boot, names_to = 'rate_trt', values_to = 'parval') %>%
-  separate(rate_trt, into = c('rate', 'trt'), sep = '_')
+# # Reproductive bootstrapped parameters
+# fr.pert.pars = fr.pert.pars %>%
+#   select(-contains('phen')) %>%
+#   pivot_longer(-boot, names_to = 'rate_trt', values_to = 'parval') %>%
+#   separate(rate_trt, into = c('rate', 'trt'), sep = '_')
 
+# Read in LTRE treatment-phenology info
+# NOTE: in some cases we will want to crop out the treatment means 
+# (e.g. when using bootstrapped means)
+trt.phen.ltre.key = merge(
+  x = read.csv('03_construct_kernels/ltre_treatment_key.csv'),
+  y = read.csv('03_construct_kernels/phen_treatment_means.csv'),
+  by.x = 'trt.phen', by.y = 'trt'
+) %>%
+  arrange(trt.phen.idx) %>%
+  select(trt.phen.idx, everything())
 
 # ------------------------------------------------------                  
 # ------ Build kernels ---------------------------------
@@ -98,8 +109,8 @@ p.germ = .001
 # --- Observed kernel
 obsv.kernel.df = merge(
   gs.obsv, fr.obsv,
-  by.x = c('size.prev', 'size.cur', 'trt', 'trt.phen', 'phen'), 
-  by.y = c('size.prev', 'size.nex', 'trt', 'trt.phen', 'phen'),
+  by.x = c('size.prev', 'size.cur', 'trt.phen.idx'), 
+  by.y = c('size.prev', 'size.nex', 'trt.phen.idx'),
 ) %>%
   mutate(
     p.size.cur = pred.surv * (pv.grow.size * (1 - prob.flower) + pf.grow.size * prob.flower) + (p.size.cur * p.germ)
@@ -118,19 +129,19 @@ obsv.pert.kernel.df = rbind(
     # need to remove some columns
     gs.obsv %>% rename(size.nex = size.cur), 
     fr.obsv.pert,
-    by = c('size.prev', 'size.nex', 'trt', 'trt.phen', 'phen')
+    by = c('size.prev', 'size.nex', 'trt.phen.idx')
   ) %>%
     select(
-      size.prev, size.nex, trt, trt.phen, phen, pv.grow.size, pf.grow.size, 
+      size.prev, size.nex, trt.phen.idx, pv.grow.size, pf.grow.size, 
       pred.surv, prob.flower, p.size.cur, param, orig.parval
     ),
   merge(
     gs.obsv.pert, 
     fr.obsv,
-    by = c('size.prev', 'size.nex', 'trt', 'trt.phen', 'phen'),
+    by = c('size.prev', 'size.nex', 'trt.phen.idx'),
   ) %>%
     select(
-      size.prev, size.nex, trt, trt.phen, phen, pv.grow.size, pf.grow.size, 
+      size.prev, size.nex, trt.phen.idx, pv.grow.size, pf.grow.size, 
       pred.surv, prob.flower, p.size.cur, param, orig.parval
     )
 ) %>%
@@ -143,47 +154,81 @@ obsv.pert.kernel.df = rbind(
 # --- Bootstrapped kernel, unperturbed
 # (this takes some time - also merging by bootstrap sample number)
 boot.kernel.df = merge(
-  gs.boot %>% pivot_longer(starts_with('b'), names_to = 'boot', values_to = 'p.size.cur'),
-  fr.boot %>% pivot_longer(starts_with('b'), names_to = 'boot', values_to = 'p.size.cur'),
-  by.x = c('size.prev', 'size.cur', 'trt', 'boot'), by.y = c('size.prev', 'size.nex', 'trt', 'boot'),
+  gs.boot, # %>% pivot_longer(starts_with('b'), names_to = 'boot', values_to = 'p.size.cur'),
+  fr.boot, # %>% pivot_longer(starts_with('b'), names_to = 'boot', values_to = 'p.size.cur'),
+  by.x = c('size.prev', 'size.cur', 'trt.phen.idx', 'boot'), by.y = c('size.prev', 'size.nex', 'trt.phen.idx', 'boot'),
   suffixes = c('.g', '.f')
 ) %>%
-  mutate(p.size.cur = p.size.cur.g + p.germ * p.size.cur.f) %>%
-  mutate(samp = gsub('b', '', boot)) %>%
-  select(-c(p.size.cur.g, p.size.cur.f, boot))
+  mutate(
+    p.size.cur = pred.surv * (pv.grow.size * (1 - prob.flower) + pf.grow.size * prob.flower) + (p.size.cur * p.germ)
+  ) %>%
+  select(-c(pred.surv, pv.grow.size, pf.grow.size, prob.flower)) %>%
+  rename(samp = boot)
+  # mutate(samp = as.numeric(gsub('b', '', boot))) %>%
+  # select(-c(p.size.cur.g, p.size.cur.f, boot))
 
 # --- Perturbed bootstrap kernel
 # (this takes even more time, because it's merging by bootstrap and perturbed
 # parameter)
 boot.pert.kernel.df = rbind(
   merge(
-    gs.boot.pert %>% 
-      pivot_longer(starts_with('b'), names_to = 'samp', values_to = 'p.size.cur') %>%
-      mutate(samp = gsub('b', '', samp)),
-    fr.boot %>% 
-      pivot_longer(starts_with('b'), names_to = 'samp', values_to = 'p.size.cur') %>%
-      mutate(samp = gsub('b', '', samp)),
-    by.x = c('size.prev', 'size.cur', 'trt', 'samp'), by.y = c('size.prev', 'size.nex', 'trt', 'samp'),
+    gs.boot.pert, # %>% 
+      # pivot_longer(starts_with('b'), names_to = 'samp', values_to = 'p.size.cur') %>%
+      # mutate(samp = gsub('b', '', samp)),
+    fr.boot, # %>% 
+      # pivot_longer(starts_with('b'), names_to = 'samp', values_to = 'p.size.cur') %>%
+      # mutate(samp = gsub('b', '', samp)),
+    by.x = c('size.prev', 'size.cur', 'trt.phen.idx', 'boot'), by.y = c('size.prev', 'size.nex', 'trt.phen.idx', 'boot'),
     suffixes = c('.g', '.f')
   ) %>%
-    select(size.prev, size.cur, trt, trt.phen, samp, param = perturb.param, p.size.cur.g, p.size.cur.f),
+    select(
+      size.prev, size.cur, trt.phen.idx, boot, param = perturb.param, 
+      pred.surv, pv.grow.size, pf.grow.size, prob.flower, p.size.cur
+    ),
   merge(
-    gs.boot %>%
-      pivot_longer(starts_with('b'), names_to = 'samp', values_to = 'p.size.cur') %>%
-      mutate(samp = gsub('b', '', samp)),
-    fr.boot.pert %>%
-      pivot_longer(starts_with('b'), names_to = 'samp', values_to = 'p.size.cur') %>%
-      mutate(samp = gsub('b', '', samp)),
-    by.x = c('size.prev', 'size.cur', 'trt', 'samp'), by.y = c('size.prev', 'size.nex', 'trt', 'samp'),
+    gs.boot, #%>%
+      # pivot_longer(starts_with('b'), names_to = 'samp', values_to = 'p.size.cur') %>%
+      # mutate(samp = gsub('b', '', samp)),
+    fr.boot.pert, # %>%
+      # pivot_longer(starts_with('b'), names_to = 'samp', values_to = 'p.size.cur') %>%
+      # mutate(samp = gsub('b', '', samp)),
+    by.x = c('size.prev', 'size.cur', 'trt.phen.idx', 'boot'), by.y = c('size.prev', 'size.nex', 'trt.phen.idx', 'boot'),
     suffixes = c('.g', '.f')
   ) %>%
-    select(size.prev, size.cur, trt, trt.phen, samp, param, p.size.cur.g, p.size.cur.f)
+    select(
+      size.prev, size.cur, trt.phen.idx, boot, param, 
+      pred.surv, pv.grow.size, pf.grow.size, prob.flower, p.size.cur
+    )
 ) %>%
-  mutate(p.size.cur = p.size.cur.g + p.size.cur.f * p.germ) %>%
-  select(-c(p.size.cur.g, p.size.cur.f))
+  mutate(
+    p.size.cur = pred.surv * (pv.grow.size * (1 - prob.flower) + pf.grow.size * prob.flower) + (p.size.cur * p.germ)
+  ) %>%
+  select(-c(pred.surv, pv.grow.size, pf.grow.size, prob.flower))
 
 # Remove input files
 rm(fr.obsv, gs.obsv, fr.boot, gs.boot, fr.boot.pert, gs.boot.pert)
+
+# Merge in treatment info and dates
+
+# Observed kernels
+obsv.kernel.df = merge(obsv.kernel.df, trt.phen.ltre.key) %>%
+  select(-trt.phen.idx) %>%
+  rename(trt = trt.rate)
+
+# Bootstrapped kernels
+boot.kernel.df = merge(boot.kernel.df, trt.phen.ltre.key %>% select(-mean.phen)) %>%
+  select(-trt.phen.idx) %>%
+  rename(trt = trt.rate)
+
+# Perturbed kernels
+obsv.pert.kernel.df = merge(obsv.pert.kernel.df, trt.phen.ltre.key) %>%
+  select(-trt.phen.idx) %>%
+  rename(trt = trt.rate)
+
+# Bootstrapped perturbed kernels
+boot.pert.kernel.df = merge(boot.pert.kernel.df, trt.phen.ltre.key %>% select(-mean.phen)) %>%
+  select(-trt.phen.idx) %>%
+  rename(trt = trt.rate)
 
 # ------------------------------------------------------                  
 # ------ Build midpoint kernels ------------------------
@@ -241,7 +286,7 @@ midp.boot.pert.kernel.df = boot.pert.kernel.df %>%
 # kernel between phen levels
 
 midp.phen.kernel.df = obsv.kernel.df %>%
-  select(-phen) %>%
+  select(-mean.phen) %>%
   # NOTE: pivoting out by trt.phen instead of trt (because we're averaging
   # across different phens)
   pivot_wider(names_from = trt.phen, values_from = p.size.cur) %>%
@@ -250,7 +295,7 @@ midp.phen.kernel.df = obsv.kernel.df %>%
   pivot_longer(c(d.c, i.c), names_to = 'contrast.phen', values_to = 'mentry', values_drop_na = TRUE)
 
 midp.phen.pert.kernel.df = obsv.pert.kernel.df %>%
-  select(-c(orig.parval, phen)) %>%
+  select(-c(orig.parval,  mean.phen)) %>%
   # subsetting out ONLY the phenology-related vital rates
   filter(grepl('phen', param)) %>%
   # NOTE: pivoting out by trt.phen instead of trt (because we're averaging
@@ -293,7 +338,7 @@ midp.phen.boot.pert.kernel.df = boot.pert.kernel.df %>%
 
 # Build data frame with observed lambdas
 obsv.lambda = split(
-  obsv.kernel.df, obsv.kernel.df[,c("trt", "trt.phen", "phen")], 
+  obsv.kernel.df, obsv.kernel.df[,c("trt", "trt.phen", "mean.phen")], 
   sep = '_', drop = TRUE
 ) %>%
   # split() splits the kernel df into a list where each entry is a data frame
@@ -303,7 +348,7 @@ obsv.lambda = split(
       df %>%
         arrange(size.prev, size.cur) %>%
         pivot_wider(names_from = size.prev, values_from = p.size.cur) %>%
-        select(-c(trt, trt.phen, phen, size.cur)) %>%
+        select(-c(trt, trt.phen, mean.phen, size.cur)) %>%
         as.matrix()
     }
   ) %>%
@@ -322,8 +367,7 @@ obsv.lambda = split(
 
 # Build data frame with bootstrapped (non-perturbed) lambdas
 boot.lambda = split(
-  boot.kernel.df,
-  boot.kernel.df[,c("trt", "trt.phen", "samp")],
+  boot.kernel.df, boot.kernel.df[,c("trt", "trt.phen", "samp")],
   sep = '_', drop = TRUE
 ) %>%
   lapply(
@@ -342,8 +386,7 @@ boot.lambda = split(
 
 # Get lambda for the midpoint of observed matrices
 midp.obsv.lambda = split(
-  midp.obsv.kernel.df,
-  midp.obsv.kernel.df[,c("contrast", 'trt.phen', "phen")],
+  midp.obsv.kernel.df, midp.obsv.kernel.df[,c("contrast", 'trt.phen', "mean.phen")],
   sep = '_', drop = TRUE
 ) %>%
   lapply(
@@ -351,7 +394,7 @@ midp.obsv.lambda = split(
       df %>%
         arrange(size.prev, size.cur) %>%
         pivot_wider(names_from = size.prev, values_from = mentry) %>%
-        select(-c(contrast, trt.phen, phen, size.cur)) %>%
+        select(-c(contrast, trt.phen, mean.phen, size.cur)) %>%
         as.matrix()
     }
   ) %>%
@@ -363,7 +406,7 @@ midp.obsv.lambda = split(
 # Get lambdas for the midpoint perturbed matrices
 midp.pert.lambda = split(
   midp.pert.kernel.df,
-  midp.pert.kernel.df[,c("contrast", 'trt.phen', 'phen', 'param')],
+  midp.pert.kernel.df[,c("contrast", 'trt.phen', 'mean.phen', 'param')],
   sep = '_', drop = TRUE
 ) %>%
   lapply(
@@ -371,7 +414,7 @@ midp.pert.lambda = split(
       df %>%
         arrange(size.prev, size.nex) %>%
         pivot_wider(names_from = size.prev, values_from = mentry) %>%
-        select(-c(contrast, trt.phen, param, phen, size.nex)) %>%
+        select(-c(contrast, trt.phen, param, mean.phen, size.nex)) %>%
         as.matrix()
     }
   ) %>%
@@ -405,7 +448,7 @@ midp.boot.lambda = split(
 # (also slow - slower than the above, takes about a minute)
 midp.boot.pert.lambda = split(
   midp.boot.pert.kernel.df, 
-  midp.boot.pert.kernel.df[,c("contrast", "trt.phen", "param", "samp")],
+  midp.boot.pert.kernel.df[,c("contrast", "trt.phen", "param", "boot")],
   sep = '_', drop = TRUE
 ) %>%
   lapply(
@@ -413,7 +456,7 @@ midp.boot.pert.lambda = split(
       df %>%
         arrange(size.prev, size.cur) %>%
         pivot_wider(names_from = size.prev, values_from = p.size.cur) %>%
-        select(-c(contrast, samp, param, trt.phen, size.cur)) %>%
+        select(-c(contrast, boot, param, trt.phen, size.cur)) %>%
         as.matrix()
     }
   ) %>%
@@ -484,7 +527,7 @@ midp.phen.boot.lambda = split(
 # Lambda for phenology midpoint bootstraps
 midp.phen.boot.pert.lambda = split(
   midp.phen.boot.pert.kernel.df,
-  midp.phen.boot.pert.kernel.df[,c("trt", "param", "contrast.phen", "samp")],
+  midp.phen.boot.pert.kernel.df[,c("trt", "param", "contrast.phen", "boot")],
   sep = '_', drop = TRUE
 ) %>%
   lapply(
@@ -492,7 +535,7 @@ midp.phen.boot.pert.lambda = split(
       df %>%
         arrange(size.prev, size.cur) %>%
         pivot_wider(names_from = size.prev, values_from = mentry) %>%
-        select(-c(trt, contrast.phen, param, samp, size.cur)) %>%
+        select(-c(trt, contrast.phen, param, boot, size.cur)) %>%
         as.matrix()
     }
   ) %>%
@@ -508,6 +551,8 @@ rm(
   midp.phen.boot.kernel.df, midp.phen.boot.pert.kernel.df, 
   midp.phen.kernel.df, midp.phen.pert.kernel.df, obsv.pert.kernel.df
 )
+
+### EVERYTHING SHOULD WORK UP UNTIL HERE!
 
 # ------------------------------------------------------                  
 # ------ Estimate sensitivities ------------------------
@@ -563,9 +608,12 @@ midp.phen.boot.sens = merge(
 # For the bootstrap dataset, these were stored in separate CSVs
 
 obsv.param.diffs = rbind(
-  gs.obsv.pert %>% distinct(trt, param, orig.parval), 
-  fr.obsv.pert %>% distinct(trt, param, orig.parval)
+  gs.obsv.pert %>% distinct(trt.phen.idx, param, orig.parval), 
+  fr.obsv.pert %>% distinct(trt.phen.idx, param, orig.parval)
 ) %>%
+  merge(trt.phen.ltre.key %>% select(-mean.phen)) %>%
+  select(-c(trt.phen.idx)) %>%
+  rename(trt = trt.rate) %>%
   # get rid of the phen 
   filter(!grepl('phen', param)) %>%
   # Get differences between treatments
@@ -574,7 +622,9 @@ obsv.param.diffs = rbind(
   select(-c(drought, irrigated, control)) %>%
   pivot_longer(c(d.c, i.c), names_to = 'contrast', values_to = 'pardiff')
 
-boot.param.diffs = rbind(gs.pert.pars, fr.pert.pars) %>%
+boot.param.diffs = merge(gs.pert.pars, fr.pert.pars, by = 'boot') %>%
+  pivot_longer(-boot, names_to = 'partrt', values_to = 'parval') %>%
+  separate_wider_delim(partrt, names = c('rate', 'trt'), delim = '_') %>%
   pivot_wider(names_from = trt, values_from = parval) %>%
   mutate(d.c = drought - control, i.c = irrigated - control) %>%
   select(-c(control, drought, irrigated)) %>%
@@ -587,8 +637,8 @@ boot.param.diffs = rbind(gs.pert.pars, fr.pert.pars) %>%
 # Okay... here we will need to introduce some negative ones
 
 obsv.phen.diffs = obsv.kernel.df %>% 
-  distinct(trt, trt.phen, phen) %>%
-  pivot_wider(names_from = trt.phen, values_from = phen) %>%
+  distinct(trt, trt.phen, mean.phen) %>%
+  pivot_wider(names_from = trt.phen, values_from = mean.phen) %>%
   mutate(
     # Need sign corrections here:
     # (d/i - control) gives the difference *from the perspective of the control*
@@ -601,8 +651,9 @@ obsv.phen.diffs = obsv.kernel.df %>%
 
 boot.phen.diffs = phen.boots %>%
   rename(samp = boot) %>%
-  mutate(phen = as.numeric(phen)) %>%
-  pivot_wider(names_from = trt.phen, values_from = phen) %>%
+  # looks like I already exported this as wide-format
+  # mutate(phen = as.numeric(phen)) %>%
+  # pivot_wider(names_from = trt.phen, values_from = phen) %>%
   mutate(d.c = drought - control, i.c = irrigated - control) %>%
   select(-c(control, drought, irrigated)) %>%
   pivot_longer(c(d.c, i.c), names_to = 'contrast.phen', values_to = 'phen.diff')
@@ -760,13 +811,13 @@ control.ltre.all = rbind(
     select(-trt.phen) %>%
     # marker for type of observation
     mutate(varb = 'alpha', samp = 'obsv', type = 'trt'),
-  # # --- Bootstrapped treatment effects
-  # boot.trt.ltre %>%
-  #   # give me LTRE values for the control dates and remove unneeded columns
-  #   filter(trt.phen %in% 'control') %>%
-  #   select(-c(trt.phen, samp)) %>%
-  #   # marker for type of observation
-  #   mutate(varb = 'alpha', samp = 'boot', type = 'trt'),
+  # --- Bootstrapped treatment effects
+  boot.trt.ltre %>%
+    # give me LTRE values for the control dates and remove unneeded columns
+    filter(trt.phen %in% 'control') %>%
+    select(-c(trt.phen, samp)) %>%
+    # marker for type of observation
+    mutate(varb = 'alpha', samp = 'boot', type = 'trt'),
   # --- Observed phenology effects (within treatment)
   obsv.phen.ltre %>%
     # give me LTRE values where the reference date is the control
@@ -775,16 +826,16 @@ control.ltre.all = rbind(
     select(-trt) %>%
     # Rename column for column agreement
     rename(contrast = contrast.phen) %>%
-    mutate(varb = 'beta', samp = 'obsv', type = 'phen')# ,
-  # # --- Bootstrapped phenology effects
-  # boot.phen.ltre %>%
-  #   # give me LTRE values where the reference date is the control
-  #   # and remove unnecessary columns
-  #   filter(trt %in% 'control') %>%
-  #   select(-c(trt, samp)) %>%
-  #   # Rename column for column agreement
-  #   rename(contrast = contrast.phen) %>%
-  #   mutate(varb = 'beta', samp = 'boot', type = 'phen')
+    mutate(varb = 'beta', samp = 'obsv', type = 'phen'),
+  # --- Bootstrapped phenology effects
+  boot.phen.ltre %>%
+    # give me LTRE values where the reference date is the control
+    # and remove unnecessary columns
+    filter(trt %in% 'control') %>%
+    select(-c(trt, samp)) %>%
+    # Rename column for column agreement
+    rename(contrast = contrast.phen) %>%
+    mutate(varb = 'beta', samp = 'boot', type = 'phen')
 ) %>%
   mutate(ltre.varb = paste0(varb, '[', rate, ']'))
 
@@ -792,7 +843,7 @@ control.ltre.all %>%
   mutate(ltre.varb = paste0(varb, '[', rate, ']')) %>%
   ggplot(aes(x = ltre.varb, y = contrib)) +
   geom_point(aes(shape = samp, alpha = samp, size = samp, colour = type)) +
-  scale_alpha_manual(values = c(0.1, 1)) +
+  scale_alpha_manual(values = c(0.5, 1)) +
   scale_shape_manual(values = c(1, 19)) +
   scale_size_manual(values = c(1, 4)) +
   scale_colour_manual(values = c('gray11', 'gray66')) +
