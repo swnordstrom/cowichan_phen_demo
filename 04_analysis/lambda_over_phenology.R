98959f81dee801ab01ee2f87952501314fdefce1
swnordstrom <swnordstrom@email.wm.edu>
2026-02-09 09:53:28 -0800

    comment out ggplot calls lol

--- 04_analysis/lambda_over_phenology.R
+++ 04_analysis/lambda_over_phenology.R
@@ -267,46 +267,46 @@ annual.phen.dates = phen.annual.mean %>%
     )
   )
 
-# Make plot
-
-lambda.trt.pan = all.lambda %>%
-  ggplot(aes(x = phen.date)) +
-  annotate(
-    'segment', linetype = 2, colour = 'gray',
-    x = min(all.lambda$phen.date), xend = max(all.lambda$phen.date),
-    y = 1, yend = 1
-  ) +
-  geom_ribbon(
-    data = all.boot.intervals,
-    aes(x = phen.date, ymin = lo, ymax = hi, fill = trt, group = trt),
-    alpha = 0.125
-  ) +
-  geom_line(aes(y = lambda, colour = trt, group = trt), linewidth = 1.2) +
-  # geom_segment(
-  #   data = ltre.boot.intervals,
-  #   aes(xend = phen.date, y = lo, yend = hi, colour = trt)
-  # ) +
-  geom_point(
-    data = annual.phen.dates,
-    aes(x = phen.date, y = ydodge, colour = trt),
-    size = 10, shape = '*'
-  ) +
-  geom_point(
-    data = ltre.lambda %>% filter(trt.rate == trt.phen),
-    aes(y = lambda, colour = trt.rate), size = 4, shape = 19
-  ) +
-  # scale_shape_manual(values = c(NA, 19)) +
-  scale_colour_manual(values = c('black', 'goldenrod1', 'dodgerblue'), '') +
-  scale_fill_manual(values = c('black', 'goldenrod1', 'dodgerblue'), '') +
-  guides(shape = 'none') +
-  labs(x = '', y = expression(lambda)) +
-  theme(
-    panel.background = element_blank(),
-    # legend.position = 'top'
-    legend.position = 'inside',
-    legend.position.inside = c(0.5, 0.85),
-    legend.direction = 'horizontal'
-  )
+# # Make plot
+# 
+# lambda.trt.pan = all.lambda %>%
+#   ggplot(aes(x = phen.date)) +
+#   annotate(
+#     'segment', linetype = 2, colour = 'gray',
+#     x = min(all.lambda$phen.date), xend = max(all.lambda$phen.date),
+#     y = 1, yend = 1
+#   ) +
+#   geom_ribbon(
+#     data = all.boot.intervals,
+#     aes(x = phen.date, ymin = lo, ymax = hi, fill = trt, group = trt),
+#     alpha = 0.125
+#   ) +
+#   geom_line(aes(y = lambda, colour = trt, group = trt), linewidth = 1.2) +
+#   # geom_segment(
+#   #   data = ltre.boot.intervals,
+#   #   aes(xend = phen.date, y = lo, yend = hi, colour = trt)
+#   # ) +
+#   geom_point(
+#     data = annual.phen.dates,
+#     aes(x = phen.date, y = ydodge, colour = trt),
+#     size = 10, shape = '*'
+#   ) +
+#   geom_point(
+#     data = ltre.lambda %>% filter(trt.rate == trt.phen),
+#     aes(y = lambda, colour = trt.rate), size = 4, shape = 19
+#   ) +
+#   # scale_shape_manual(values = c(NA, 19)) +
+#   scale_colour_manual(values = c('black', 'goldenrod1', 'dodgerblue'), '') +
+#   scale_fill_manual(values = c('black', 'goldenrod1', 'dodgerblue'), '') +
+#   guides(shape = 'none') +
+#   labs(x = '', y = expression(lambda)) +
+#   theme(
+#     panel.background = element_blank(),
+#     # legend.position = 'top'
+#     legend.position = 'inside',
+#     legend.position.inside = c(0.5, 0.85),
+#     legend.direction = 'horizontal'
+#   )
 
 # lambda.trt.pan
 
@@ -335,84 +335,84 @@ boot.lambda.diff.interval = boot.lambda.diff %>%
       pivot_longer(c(d.c, i.c), names_to = 'contrast', values_to = 'mean.d.lambda')
   )
 
-lambda.contr.pan = boot.lambda.diff %>%
-  filter(as.numeric(gsub('b', '', boot)) < 101) %>%
-  mutate(
-    # contrast = paste(ifelse(contrast %in% 'd.c', 'drought', 'irrigated'), 'vs. control')
-    contrast = ifelse(
-      contrast %in% 'd.c',
-      'i) drought vs. control',
-      'ii) irrigated vs. control'
-    )
-  ) %>%
-  ggplot(aes(x = phen.date, group = contrast)) +
-  annotate(
-    'segment',
-    x = as.Date('1970-04-22'), xend = as.Date('1970-05-20'),
-    y = 0, yend = 0,
-    linetype = 2, colour = 'gray'
-  ) +
-  geom_point(
-    aes(y = d.lambda), 
-    position = position_jitter(width = 1), alpha = 0.125
-  ) +
-  geom_point(
-    data = boot.lambda.diff.interval %>%
-      mutate(
-        # contrast = paste(ifelse(contrast %in% 'd.c', 'drought', 'irrigated'), 'vs. control')
-        contrast = ifelse(
-          contrast %in% 'd.c',
-          'i) drought vs. control',
-          'ii) irrigated vs. control'
-        )
-      ),
-    aes(y = mean.d.lambda),
-    size = 4, shape = 21, stroke = 2
-  ) +
-  geom_point(
-    # y limits here found by:
-    # ggplot_build(lambda.contr.pan)$layout$panel_scales_y
-    data = annual.phen.dates %>%
-      filter(!(trt %in% 'control')) %>%
-      mutate(
-        # contrast - paste(trt, 'vs. control')),
-        contrast = ifelse(
-          trt %in% 'drought',
-          'i) drought vs. control',
-          'ii) irrigated vs. control'
-        )
-      ),
-    aes(x = phen.date, y = -0.007, colour = contrast),
-    shape = '*', size = 10
-  ) +
-  geom_segment(
-    data = boot.lambda.diff.interval %>%
-      mutate(
-        # contrast = paste(ifelse(contrast %in% 'd.c', 'drought', 'irrigated'), 'vs. control')
-        contrast = ifelse(
-          contrast %in% 'd.c',
-          'i) drought vs. control',
-          'ii) irrigated vs. control'
-        )
-      ),
-    aes(xend = phen.date, y = lo, yend = hi)
-  ) +
-  # scale_shape_manual(values = c(1, 19)) +
-  labs(x = '', y = expression(Delta~lambda)) +
-  guides(colour = 'none', fill = 'none') +
-  # scale_colour_manual(values = c('red', 'blue')) +
-  # scale_fill_manual(values = c('red', 'blue')) +
-  scale_colour_manual(values = c('goldenrod1', 'dodgerblue')) +
-  scale_x_continuous(
-    breaks = as.Date(c('1970-04-27', '1970-05-04', '1970-05-11', '1970-05-18')),
-    labels = format(as.Date(c('1970-04-27', '1970-05-04', '1970-05-11', '1970-05-18')), '%b %d')
-  ) +
-  facet_wrap(~ contrast, nrow = 2) +
-  theme(
-    panel.background = element_blank(),
-    strip.background = element_part_rect(fill = 'white', side = 'b', colour = 'gray22'),
-    legend.position = 'none'
-  )
+# lambda.contr.pan = boot.lambda.diff %>%
+#   filter(as.numeric(gsub('b', '', boot)) < 101) %>%
+#   mutate(
+#     # contrast = paste(ifelse(contrast %in% 'd.c', 'drought', 'irrigated'), 'vs. control')
+#     contrast = ifelse(
+#       contrast %in% 'd.c',
+#       'i) drought vs. control',
+#       'ii) irrigated vs. control'
+#     )
+#   ) %>%
+#   ggplot(aes(x = phen.date, group = contrast)) +
+#   annotate(
+#     'segment',
+#     x = as.Date('1970-04-22'), xend = as.Date('1970-05-20'),
+#     y = 0, yend = 0,
+#     linetype = 2, colour = 'gray'
+#   ) +
+#   geom_point(
+#     aes(y = d.lambda), 
+#     position = position_jitter(width = 1), alpha = 0.125
+#   ) +
+#   geom_point(
+#     data = boot.lambda.diff.interval %>%
+#       mutate(
+#         # contrast = paste(ifelse(contrast %in% 'd.c', 'drought', 'irrigated'), 'vs. control')
+#         contrast = ifelse(
+#           contrast %in% 'd.c',
+#           'i) drought vs. control',
+#           'ii) irrigated vs. control'
+#         )
+#       ),
+#     aes(y = mean.d.lambda),
+#     size = 4, shape = 21, stroke = 2
+#   ) +
+#   geom_point(
+#     # y limits here found by:
+#     # ggplot_build(lambda.contr.pan)$layout$panel_scales_y
+#     data = annual.phen.dates %>%
+#       filter(!(trt %in% 'control')) %>%
+#       mutate(
+#         # contrast - paste(trt, 'vs. control')),
+#         contrast = ifelse(
+#           trt %in% 'drought',
+#           'i) drought vs. control',
+#           'ii) irrigated vs. control'
+#         )
+#       ),
+#     aes(x = phen.date, y = -0.007, colour = contrast),
+#     shape = '*', size = 10
+#   ) +
+#   geom_segment(
+#     data = boot.lambda.diff.interval %>%
+#       mutate(
+#         # contrast = paste(ifelse(contrast %in% 'd.c', 'drought', 'irrigated'), 'vs. control')
+#         contrast = ifelse(
+#           contrast %in% 'd.c',
+#           'i) drought vs. control',
+#           'ii) irrigated vs. control'
+#         )
+#       ),
+#     aes(xend = phen.date, y = lo, yend = hi)
+#   ) +
+#   # scale_shape_manual(values = c(1, 19)) +
+#   labs(x = '', y = expression(Delta~lambda)) +
+#   guides(colour = 'none', fill = 'none') +
+#   # scale_colour_manual(values = c('red', 'blue')) +
+#   # scale_fill_manual(values = c('red', 'blue')) +
+#   scale_colour_manual(values = c('goldenrod1', 'dodgerblue')) +
+#   scale_x_continuous(
+#     breaks = as.Date(c('1970-04-27', '1970-05-04', '1970-05-11', '1970-05-18')),
+#     labels = format(as.Date(c('1970-04-27', '1970-05-04', '1970-05-11', '1970-05-18')), '%b %d')
+#   ) +
+#   facet_wrap(~ contrast, nrow = 2) +
+#   theme(
+#     panel.background = element_blank(),
+#     strip.background = element_part_rect(fill = 'white', side = 'b', colour = 'gray22'),
+#     legend.position = 'none'
+#   )
 
 # lambda.legend = get_plot_component(
 #   lambda.trt.pan + theme(legend.position = 'top'),
